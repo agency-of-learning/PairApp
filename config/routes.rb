edebe540c7c436cb76de9974aea7073a201848b2
Dave Paola <dpaola2@gmail.com>
2023-02-28 17:13:55 -0800

    accept and unaccept

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  resources :pair_requests
   # Shortcuts
   get 'landing/index'
   get '/auth/auth0/callback' => 'auth0#callback'
   get '/auth/failure' => 'auth0#failure'
   get '/auth/logout' => 'auth0#logout'
-
+  
   # Alphabetized Routes
+  resources :pair_requests do
+    resources :acceptances, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
 
   # Root  
