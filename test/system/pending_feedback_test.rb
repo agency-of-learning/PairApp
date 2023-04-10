5514365f7ed87cfd36f29c1ee8ff19d4e7dc6349
Gustavo Valenzuela <GALTdea@gmail.com>
2023-04-10 10:52:26 -0700

    WIP:  Adds system test file for pending_feedback feature. This commits lays out a strategy to execute the test

---
+++ test/system/pending_feedback_test.rb
@@ -0,0 +1,38 @@
+require "application_system_test_case"
+# class PairRequestsTest < ApplicationSystemTestCase
+
+class PendingFeedbackTest < ApplicationSystemTestCase
+  
+  #step 1: set up a pair request with no feedback
+  
+  setup do
+    # pair_requests method is calling the variable :one from the fixtures file pair_requests.yml
+    @pair_request = pair_requests(:one)
+    @pair_request.update(author_overall_rating: nil, acceptor_overall_rating: nil)
+  end
+
+  # setp 2: visit the index page
+  test "visiting the index" do
+    visit pair_requests_url
+    assert_selector "h1", text: "Pair requests"
+  end
+
+  # step 3: click on the add feedback button, link
+
+
+  # step 4: if user == auther, it should show feedback form for author, if user == acceptor, it should show feedback form for acceptor
+
+
+  # step 5: user fill in the form
+
+  # step 6: user  clicks on submit
+
+
+  # step 7: 
+    # a) user should be redirected to the pair request show page
+    # b) the pair request should have feedback from the users who submitted the form
+    # c) the pair request should no longer be in the pending feedback list
+
+
+
+end
