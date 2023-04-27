b88304af60e4a885c2c441d97993e645e3f86693
Josh Smith <jmsmith1018@gmail.com>
2023-04-27 10:18:26 -0600

    Remove devise's route for new registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:registrations]
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', as: 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', as: 'user_registration'
+  end
+
   # Root
   root to: 'landing#index'
   # Shortcuts
