3ce3985ec1e02d6bac1a08f32d4bcf4972f3cfbe
Josh Smith <jmsmith1018@gmail.com>
2023-05-08 11:33:10 -0600

    Use methods that respect model validations for accepting/rejecting

--- app/controllers/pair_requests/acceptances_controller.rb
+++ app/controllers/pair_requests/acceptances_controller.rb
@@ -1,7 +1,7 @@
 class PairRequests::AcceptancesController < ApplicationController
   def create
     @pair_request = PairRequest.find(params[:pair_request_id])
-    @pair_request.update_attribute(:status, :accepted)
+    @pair_request.accepted!
 
     flash[:notice] = 'You have accepted this pair request.'
     redirect_to pair_request_path(@pair_request)
--- app/controllers/pair_requests/rejections_controller.rb
+++ app/controllers/pair_requests/rejections_controller.rb
@@ -1,11 +1,9 @@
 class PairRequests::RejectionsController < ApplicationController
   def create
-    # @pair_request = PairRequest.find(params[:pair_request_id])
     @pair_request = PairRequest.find(params[:pair_request_id])
-    @pair_request.update_attribute(:status, :rejected)
+    @pair_request.rejected!
 
     flash[:notice] = 'You have rejected this pair request.'
-
     redirect_to pair_request_path(@pair_request)
   end
 end
