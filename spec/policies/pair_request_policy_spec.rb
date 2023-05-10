4453649e853ffe9b432ad54cb392471b7ac14cda
Josh Smith <jmsmith1018@gmail.com>
2023-05-09 22:39:21 -0600

    Memoize repeated variables in policy spec

--- spec/policies/pair_request_policy_spec.rb
+++ spec/policies/pair_request_policy_spec.rb
@@ -1,50 +1,52 @@
 RSpec.describe PairRequestPolicy do
   subject { described_class }
 
+  let(:user) { build(:user) }
+  let(:admin) { build(:user, :admin) }
+
+  let(:not_owned_request) { build(:pair_request) }
+  let(:authored_request) { build(:pair_request, author: user) }
+  let(:invited_request) { build(:pair_request, invitee: user) }
+  let(:invited_expired_request) { build(:pair_request, invitee: user, status: :expired) }
+
   permissions :index?, :show? do
     it 'denies access if the user is not the author, invitee, or an admin account' do
-      expect(subject).not_to permit(build(:user), build(:pair_request))
+      expect(subject).not_to permit(user, not_owned_request)
     end
 
     it 'grants access if the user is the author' do
-      authoring_user = build(:user)
-      expect(subject).to permit(authoring_user, build(:pair_request, author: authoring_user))
+      expect(subject).to permit(user, authored_request)
     end
 
     it 'grants access if the user is the invitee' do
-      invitee_user = build(:user)
-      expect(subject).to permit(invitee_user, build(:pair_request, invitee: invitee_user))
+      expect(subject).to permit(user, invited_request)
     end
 
     it 'grants access if the user is an admin' do
-      admin = build(:user, :admin)
-      expect(subject).to permit(admin, build(:pair_request))
+      expect(subject).to permit(admin, not_owned_request)
     end
   end
 
   permissions :new?, :create? do
     it 'grants access to the user' do
-      expect(subject).to permit(build(:user), build(:pair_request))
+      expect(subject).to permit(user, not_owned_request)
     end
   end
 
   permissions :accept? do
     context 'when the user is not the invitee' do
       it 'denies access' do
-        authoring_user = build(:user)
-        expect(subject).not_to permit(authoring_user, build(:pair_request, author: authoring_user))
+        expect(subject).not_to permit(user, authored_request)
       end
     end
 
     context 'when the user is the invitee' do
-      let(:invitee) { build(:user) }
-
       it 'denies access if the request is expired/rejected' do
-        expect(subject).not_to permit(invitee, build(:pair_request, invitee:, status: :expired))
+        expect(subject).not_to permit(user, invited_expired_request)
       end
 
       it 'grants access if the user is the invitee and the request is pending' do
-        expect(subject).to permit(invitee, build(:pair_request, invitee:))
+        expect(subject).to permit(user, invited_request)
       end
     end
   end
@@ -52,22 +54,17 @@ RSpec.describe PairRequestPolicy do
   permissions :reject? do
     context 'when the user is an the owner' do
       it 'denies access' do
-        expect(subject).not_to permit(build(:user), build(:pair_request))
+        expect(subject).not_to permit(user, not_owned_request)
       end
     end
 
     context 'when the user is an owner' do
-      let(:owner) { build(:user) }
-
       it "denies access if the request isn't accepted or pending" do
-        expect(subject).not_to permit(
-          owner,
-          build(:pair_request, author: owner, status: :expired)
-        )
+        expect(subject).not_to permit(user, invited_expired_request)
       end
 
       it 'grants access if the request is pending/accepted' do
-        expect(subject).to permit(owner, build(:pair_request, invitee: owner))
+        expect(subject).to permit(user, authored_request)
       end
     end
   end
