8fefb0ec19db63876b78c79627effb4ddc501884
Josh Smith <jmsmith1018@gmail.com>
2023-05-09 23:18:45 -0600

    Wrap user scope specs in a `describe 'scopes'` block

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,49 +20,51 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  describe '::invitee_select_for' do
-    let!(:param_user) { create(:user) }
-    let!(:other_user) { create(:user) }
+  describe 'scopes' do
+    describe '.invitee_select_for' do
+      let!(:param_user) { create(:user) }
+      let!(:other_user) { create(:user) }
 
-    it 'excludes the user in the param' do
-      param_user_data = [param_user.email, param_user.id]
-      expect(described_class.invitee_select_for(param_user)).not_to include(param_user_data)
+      it 'excludes the user in the param' do
+        param_user_data = [param_user.email, param_user.id]
+        expect(described_class.invitee_select_for(param_user)).not_to include(param_user_data)
+      end
+
+      it 'includes other users' do
+        other_user_data = [other_user.email, other_user.id]
+        expect(described_class.invitee_select_for(param_user)).to include(other_user_data)
+      end
     end
 
-    it 'includes other users' do
-      other_user_data = [other_user.email, other_user.id]
-      expect(described_class.invitee_select_for(param_user)).to include(other_user_data)
-    end
-  end
+    describe '#all_pair_requests' do
+      let!(:user) { create(:user) }
+      let!(:authored_request) { create(:pair_request, author: user) }
+      let!(:received_request) { create(:pair_request, invitee: user) }
 
-  describe '#all_pair_requests' do
-    let!(:user) { create(:user) }
-    let!(:authored_request) { create(:pair_request, author: user) }
-    let!(:received_request) { create(:pair_request, invitee: user) }
+      it "fetches a user's received and authored pair requests" do
+        expect(user.all_pair_requests).to include(authored_request, received_request)
+      end
 
-    it "fetches a user's received and authored pair requests" do
-      expect(user.all_pair_requests).to include(authored_request, received_request)
-    end
+      it "doesn't return include user's requests" do
+        other_request = create(:pair_request)
+        expect(user.all_pair_requests).not_to include(other_request)
+      end
 
-    it "doesn't return include user's requests" do
-      other_request = create(:pair_request)
-      expect(user.all_pair_requests).not_to include(other_request)
-    end
-
-    it 'returns an ActiveRecord Relation' do
-      expect(user.all_pair_requests).to be_a ActiveRecord::Relation
-    end
-  end
-
-  describe '#role' do
-    let(:user) { build(:user) }
-
-    it 'defaults to member' do
-      expect(user.role).to eq('member')
+      it 'returns an ActiveRecord Relation' do
+        expect(user.all_pair_requests).to be_a ActiveRecord::Relation
+      end
     end
 
-    it 'responds to member? properly' do
-      expect(user.member?).to be(true)
+    describe '#role' do
+      let(:user) { build(:user) }
+
+      it 'defaults to member' do
+        expect(user.role).to eq('member')
+      end
+
+      it 'responds to member? properly' do
+        expect(user.member?).to be(true)
+      end
     end
   end
 end
