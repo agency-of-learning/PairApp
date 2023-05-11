fb4741b6d8184bd0dc234b755419ee31178c516f
Daniel Rassiner <daniel@rassiner.com>
2023-05-11 15:05:56 -0700

    remove some boilerplate files that are not needed (#41)
    * remove some boilerplate files that are not needed
    
    * disable jbuilder

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,10 @@ module PairApp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     config.generators.test_framework = :rspec
+
+    # Disable jbuilder when running rails g scaffold
+    config.generators.jbuilder = false
+
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
