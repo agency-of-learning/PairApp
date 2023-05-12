2964983b6ff729de940819ba0694657c598a5af1
Daniel Rassiner <daniel@rassiner.com>
2023-05-12 14:11:20 -0700

    add paper_trail versioning to PairRequest (#43)

--- spec/models/pair_request_spec.rb
+++ spec/models/pair_request_spec.rb
@@ -28,7 +28,58 @@ RSpec.describe PairRequest do
   subject { build(:pair_request, duration:, when: when_date) }
 
   let(:duration) { 20.minutes }
-  let(:when_date) { Date.today + 2.weeks }
+  let(:when_date) { Date.current + 2.weeks }
+
+  describe '#versioning' do
+    # NOTE: this is a helper method provided by the paper_trail gem
+    with_versioning do
+      before do
+        subject.save!
+      end
+
+      it 'enables paper_trail' do
+        expect(subject).to be_versioned
+      end
+
+      context 'when created' do
+        it 'creates a version' do
+          expect(subject.versions.count).to eq(1)
+        end
+
+        it 'creates a version with the correct event' do
+          expect(subject.versions.last.event).to eq('create')
+        end
+      end
+
+      context 'when updated' do
+        before do
+          subject.accepted!
+        end
+
+        it 'creates another version' do
+          expect(subject.versions.count).to eq(2)
+        end
+
+        it 'creates a version with the correct event' do
+          expect(subject.versions.last.event).to eq('update')
+        end
+      end
+
+      context 'when deleted' do
+        before do
+          subject.destroy
+        end
+
+        it 'creates another version' do
+          expect(subject.versions.count).to eq(2)
+        end
+
+        it 'creates a version with the correct event' do
+          expect(subject.versions.last.event).to eq('destroy')
+        end
+      end
+    end
+  end
 
   describe '#duration' do
     it 'is valid' do
@@ -50,7 +101,7 @@ RSpec.describe PairRequest do
     end
 
     context 'when date is more than a month into the future' do
-      let(:when_date) { Date.today + 2.months }
+      let(:when_date) { Date.current + 2.months }
 
       it 'is invalid' do
         expect(subject).not_to be_valid
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+require 'paper_trail/frameworks/rspec'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
