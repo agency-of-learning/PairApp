cf134e506a3dee7ab11cddb344e7bc09eeb4de98
Daniel Rassiner <daniel@rassiner.com>
2023-05-12 10:58:24 -0700

    add sendgrid (#38)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,5 +69,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   # NOTE: integrate with letter_opener.
+
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
