bc48eae6336581b8ac2b85e7c689338be01187fb
Gustavo Valenzuela <GALTdea@gmail.com>
2023-05-16 11:30:13 -0700

    Adds time_zone select helper to invitations, adds params sanitizer to application controller co-authored-by: @dvsconcept1986

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,21 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
   include ApplicationHelper
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
   # PaperTrail helper for getting current_user
   before_action :set_paper_trail_whodunnit
 
-  # around_action :set_time_zone, if: :current_user
+  around_action :set_time_zone, if: :current_user
 
   private
-
   def set_time_zone(&)
     Time.use_zone(current_user.time_zone, &)
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:accept_invitation, keys: [:time_zone])
   end
 end
