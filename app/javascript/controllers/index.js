91336f33014a52bd8e506fd6614967b51e7739cb
Ariel Fogel <fogeltine@gmail.com>
2023-05-18 03:47:46 +0300

    Migrate to esbuild (#57)
    * add and install esbuild
    
    * remove import maps
    
    * git ignore node modules? autoadded by esbuild install
    
    * update README to reflect js dependencies
    
    * sassc dev only
    
    ---------
    
    Co-authored-by: toyhammered <daniel@rassiner.com>

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,11 +1,15 @@
-// Import and register all your controllers from the importmap under controllers/*
+import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components";
+import { application } from './application';
 
-import { application } from "controllers/application"
+// Register each new controller with Stimulus
+import hello_controller from './hello_controller.js';
+application.register(hello_controller.name, hello_controller.module.default);
 
-// Eager load all controllers defined in the import map under controllers/**/*_controller
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
-
-// Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
-// import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
-// lazyLoadControllersFrom("controllers", application)
+application.register('alert', Alert);
+application.register('autosave', Autosave);
+application.register('dropdown', Dropdown);
+application.register('modal', Modal);
+application.register('tabs', Tabs);
+application.register('popover', Popover);
+application.register('toggle', Toggle);
+application.register('slideover', Slideover);
