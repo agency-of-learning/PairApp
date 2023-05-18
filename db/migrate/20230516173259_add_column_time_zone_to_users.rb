92da937dd31ae522e5c70aedba48d7bc16a50bbf
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2023-05-17 20:06:53 -0700

    Pr1 timezone (#58)
    * Adds migration file to add time_zone column to users table
    co-authored-by: @dvsconcept1986 <
    
    * Adds time_zone select helper to invitations, adds
    params sanitizer to application controller
    co-authored-by: @dvsconcept1986
    
    * Adds additional files related to time_zone migration
    
    co-authored-by: @dvsconcept1986
    
    * breaking down branch- pr1-timezone
    
    * Adds edits to the migration file to add User
    backfilling

--- db/migrate/20230516173259_add_column_time_zone_to_users.rb
+++ db/migrate/20230516173259_add_column_time_zone_to_users.rb
@@ -2,4 +2,13 @@ class AddColumnTimeZoneToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :time_zone, :string, default: 'UTC', null: false
   end
+  def up
+    add_column :users, :time_zone, :string, default: 'UTC'
+    User.update_all(time_zone: 'UTC')
+    change_column :users, :time_zone, :string, null: false
+   end
+   
+   def down
+    remove_column :users, :time_zone
+   end
 end
