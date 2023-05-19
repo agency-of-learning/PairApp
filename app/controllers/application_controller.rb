5a5e5a066b87095f4db23509b4451f73df9faaf6
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2023-05-18 18:14:48 -0700

    Pr2 timezone (#59)
    adds client dropdown for timezone
    ---------
    co-authored-by: @dvsconcept1986

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,21 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
   include ApplicationHelper
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
   # PaperTrail helper for getting current_user
   before_action :set_paper_trail_whodunnit
 
-  # around_action :set_time_zone, if: :current_user
+  around_action :set_time_zone, if: :current_user
 
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:accept_invitation, keys: [:time_zone])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:time_zone])
+  end
+  
   private
   def set_time_zone(&)
     Time.use_zone(current_user.time_zone, &)
