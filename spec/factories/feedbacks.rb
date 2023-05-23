68e522f7200da3e4657d18e436d99e33041a96b1
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2023-05-23 11:02:41 -0700

    Create a feedback model with associations (#62)
    * Create a feedback model with associations
    
    Co-authored-by: @JoshDevHub jmsmith1018@gmail.com
    
    * Adds feedback FactoryBot, adds feedback enum
    
    co-authored-by: @JoshDevHub jmsmith1018@gmail.com

---
+++ spec/factories/feedbacks.rb
@@ -0,0 +1,38 @@
+# == Schema Information
+#
+# Table name: feedbacks
+#
+#  id                 :bigint           not null, primary key
+#  data               :jsonb
+#  locked_at          :datetime
+#  overall_rating     :integer          default(0), not null
+#  referenceable_type :string           not null
+#  status             :integer          default(0), not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  author_id          :bigint           not null
+#  receiver_id        :bigint           not null
+#  referenceable_id   :bigint           not null
+#
+# Indexes
+#
+#  index_feedbacks_on_author_id      (author_id)
+#  index_feedbacks_on_receiver_id    (receiver_id)
+#  index_feedbacks_on_referenceable  (referenceable_type,referenceable_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (author_id => users.id)
+#  fk_rails_...  (receiver_id => users.id)
+#
+FactoryBot.define do
+  factory :feedback do
+    association :author, factory: :user
+    association :receiver, factory: :user
+    association :referenceable, factory: :pair_request
+    overall_rating { 1 }
+    status { 1 }
+    data { {} }
+    locked_at { "MyString" }
+  end
+end
