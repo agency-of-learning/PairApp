3790591cda2ab379811d0a0249b04d5843991ee8
toyhammered <daniel@rassiner.com>
2023-05-29 21:24:16 -0700

    get annotation

--- app/models/standup_meeting_group_user.rb
+++ app/models/standup_meeting_group_user.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: standup_meeting_groups_users
+#
+#  id                       :bigint           not null, primary key
+#  created_at               :datetime         not null
+#  updated_at               :datetime         not null
+#  standup_meeting_group_id :bigint           not null
+#  user_id                  :bigint           not null
+#
+# Indexes
+#
+#  index_smg_users_on_smg_id_and_user_id  (standup_meeting_group_id,user_id)
+#  index_smg_users_on_user_id_and_smg_id  (user_id,standup_meeting_group_id)
+#
 class StandupMeetingGroupUser < ApplicationRecord
   self.table_name = 'standup_meeting_groups_users'
 
