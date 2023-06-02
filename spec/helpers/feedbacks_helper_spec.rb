35f7ce508ac9d8507d306dc8357800894cfc5aa3
Gustavo Valenzuela <GALTdea@gmail.com>
2023-06-02 12:11:20 -0700

    WIP: Adds specs for feedbacks_helper and modifies pair_requests factory to status: :completed option.

--- spec/helpers/feedbacks_helper_spec.rb
+++ spec/helpers/feedbacks_helper_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
-
-# Specs in this file have access to a helper object that includes
-# the FeedbacksHelper. For example:
-#
-# describe FeedbacksHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe FeedbacksHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'pending_feedback_link' do
+    let(:pair_request_author) { create(:user) }
+    let(:pair_request_invitee) { create(:user) }
+
+    let(:pair_request) { create(:pair_request, author: pair_request_author, invitee: pair_request_invitee, status: :completed) }
+ 
+    context "when the feedbac link is present and current_user == feedback.author" do
+      it 'returns a link to the feedback' do
+        binding.pry
+        link = helper.pending_feedback_link(pair_request, pair_request_author)
+        expect(link).to eq("<a class=\"btn bg-red-500\" href=\"/feedbacks/#{feedback.id}/edit\">Submit Feedback</a>")
+      end
+    end
+  end
 end
