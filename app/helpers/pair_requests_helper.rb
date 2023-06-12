b0fe50ce631fccd5a619b131d2e649814a773932
Josh Smith <jmsmith1018@gmail.com>
2023-06-11 19:26:20 -0700

    Rework pair request date format logic to better fit wireframe
    Additionally begin storing date formats in a more conventional way

--- app/helpers/pair_requests_helper.rb
+++ app/helpers/pair_requests_helper.rb
@@ -1,5 +1,12 @@
 module PairRequestsHelper
-  def format_date(pair_request)
-    pair_request.when.strftime('%B %-e, %Y %-l:%M%P')
+  def format_request_date(pair_request)
+    start_time = pair_request.when
+    end_time = start_time + pair_request.duration
+
+    format_date = start_time.to_fs(:month_day_year)
+    format_time = start_time.to_fs(:hour_min_period)
+    time_range = "#{format_time} - #{end_time.to_fs(:hour_min_period)}"
+
+    content_tag(:span, format_date) + tag.br + content_tag(:span, time_range)
   end
 end
--- spec/helpers/pair_requests_helper_spec.rb
+++ spec/helpers/pair_requests_helper_spec.rb
@@ -1,24 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe PairRequestsHelper do
-  let(:pair_request) { build(:pair_request, when: when_date) }
+  let(:pair_request) { build(:pair_request, when: when_date, duration:) }
 
-  describe '#format_date' do
+  describe '#format_request_date' do
     context 'when the date is June 10, 2023 at 11:40am' do
       let(:when_date) { DateTime.parse('2023-06-10T11:40:00+00:00') }
+      let(:duration) { 30.minutes }
 
       it 'outputs in the expected format' do
-        expected_output = 'June 10, 2023 11:40am'
-        expect(format_date(pair_request)).to eq(expected_output)
+        expected_output = '<span>June 10, 2023</span><br><span>11:40am - 12:10pm</span>'
+        expect(format_request_date(pair_request)).to eq(expected_output)
       end
     end
 
     context 'when the date is August 1, 2023 at 3:12pm' do
       let(:when_date) { DateTime.parse('2023-08-01T15:12:00+00:00') }
+      let(:duration) { 60.minutes }
 
       it 'outputs in the expected format' do
-        expected_output = 'August 1, 2023 3:12pm'
-        expect(format_date(pair_request)).to eq(expected_output)
+        expected_output = '<span>August 1, 2023</span><br><span>3:12pm - 4:12pm</span>'
+        expect(format_request_date(pair_request)).to eq(expected_output)
       end
     end
   end
