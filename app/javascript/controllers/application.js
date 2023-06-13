1100433a458eda3869ed63d8500ac9acad05aba1
Eric Halverson <ericleifhalverson@gmail.com>
2023-06-13 15:42:42 -0700

    Remove 'hello world' controller, add 'alert' controller with 'close' action, move the 'TailwindCSS components from the 'index' controller to 'application' controller

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,16 @@
 import { Application } from "@hotwired/stimulus";
 
 const application = Application.start();
+import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
+
+application.register('alert', Alert);
+application.register('autosave', Autosave);
+application.register('dropdown', Dropdown);
+application.register('modal', Modal);
+application.register('tabs', Tabs);
+application.register('popover', Popover);
+application.register('toggle', Toggle);
+application.register('slideover', Slideover);
 
 // Configure Stimulus development experience;
 application.debug = true;
