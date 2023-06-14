cc042a63da3742cb295eab2b8d4085c00bd87422
toyhammered <daniel@rassiner.com>
2023-06-13 17:40:04 -0700

    fix admin constraint

--- lib/admin_constraint.rb
+++ lib/admin_constraint.rb
@@ -2,7 +2,8 @@ class AdminConstraint
   def self.matches?(request)
     return false if request.session['warden.user.user.key'].blank?
 
-    user_id, _key = request.session['warden.user.user.key']
+    user_id_array, _key = request.session['warden.user.user.key']
+    user_id = user_id_array.first
 
     user = User.find(user_id)
     user.admin?
