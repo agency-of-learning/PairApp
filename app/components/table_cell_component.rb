b3066ec90ac1d4104a152c532659d083b2b94950
Ariel Fogel <fogeltine@gmail.com>
2023-06-16 15:38:53 -0400

    hide action column on mobile to improve readability

--- app/components/table_cell_component.rb
+++ app/components/table_cell_component.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class TableCellComponent < ViewComponent::Base
-  def initialize(container_link: nil)
+  def initialize(container_link: nil, hidden_on_mobile: false)
     @container_link = container_link
+    @hidden_on_mobile = hidden_on_mobile
   end
 
   private
 
-  attr_reader :container_link
+  attr_reader :container_link, :hidden_on_mobile
 end
