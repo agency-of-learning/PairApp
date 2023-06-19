ad30c2fb40e76e8486a7e25d26d88c6b7cade544
Ariel Fogel <fogeltine@gmail.com>
2023-06-19 11:42:25 -0400

    update table cell component to use params as classes rather than use flag

--- app/components/table_cell_component.rb
+++ app/components/table_cell_component.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class TableCellComponent < ViewComponent::Base
-  def initialize(container_link: nil, hidden_on_mobile: false)
+  def initialize(container_link: nil, classes: nil)
     @container_link = container_link
-    @hidden_on_mobile = hidden_on_mobile
+    @classes = classes
   end
 
   private
 
-  attr_reader :container_link, :hidden_on_mobile
+  attr_reader :container_link, :classes
 end
