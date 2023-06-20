768d380af5456f943353a5c0e0c038cc13a6d07d
Ariel Fogel <fogeltine@gmail.com>
2023-06-20 16:13:34 -0400

    refactor rejection policy to only grant access to invitee

--- app/policies/pair_request/rejection_policy.rb
+++ app/policies/pair_request/rejection_policy.rb
@@ -2,12 +2,6 @@ class PairRequest::RejectionPolicy < ApplicationPolicy
   alias_method :pair_request, :record
 
   def create?
-    user_is_owner? && (pair_request.pending? || pair_request.accepted?)
-  end
-
-  private
-
-  def user_is_owner?
-    pair_request.author == user || pair_request.invitee == user
+    pair_request.invitee == user && (pair_request.pending? || pair_request.accepted?)
   end
 end
