bf350abed99ad851f7c0d00294fa3cc2bd7cb820
Ariel Fogel <fogeltine@gmail.com>
2023-06-19 21:11:01 -0400

    rename param from classes to class_names

--- app/components/table_cell_component.rb
+++ app/components/table_cell_component.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class TableCellComponent < ViewComponent::Base
-  def initialize(container_link: nil, classes: 'table-cell')
+  def initialize(container_link: nil, class_names: 'table-cell')
     @container_link = container_link
-    @classes = classes
+    @class_names = class_names
   end
 
   private
 
-  attr_reader :container_link, :classes
+  attr_reader :container_link, :class_names
 end
