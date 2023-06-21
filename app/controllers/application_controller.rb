d7dc5d020a7221a3d52ea5ef1a8e6b21e5321bae
Josh Smith <jmsmith1018@gmail.com>
2023-06-20 22:53:37 -0700

    Register new flash type for form errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,24 +2,26 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
   include ApplicationHelper
 
+  add_flash_types :form_errors
+
   before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-
 
   # PaperTrail helper for getting current_user
   before_action :set_paper_trail_whodunnit
 
   around_action :set_time_zone, if: :current_user
 
-
   protected
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:accept_invitation, keys: [:time_zone])
     devise_parameter_sanitizer.permit(:account_update, keys: [:time_zone])
   end
-  
+
   private
+
   def set_time_zone(&)
     Time.use_zone(current_user.time_zone, &)
   end
