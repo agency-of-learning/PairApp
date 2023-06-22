0c4d5c4f9e7cf964862a35267ba0d5609fef2a29
toyhammered <daniel@rassiner.com>
2023-06-21 21:09:10 -0700

    get turbo working.

--- app/controllers/standup_meeting_groups/joins_controller.rb
+++ app/controllers/standup_meeting_groups/joins_controller.rb
@@ -6,17 +6,37 @@ class StandupMeetingGroups::JoinsController < ApplicationController
     authorize @standup_meeting_group_user, policy_class: StandupMeetingGroup::JoinPolicy
 
     @standup_meeting_group_user.save
-    flash[:notice] = 'You have joined this standup meeting group.'
-    redirect_to standup_meeting_group_path(@standup_meeting_group)
+    # flash[:notice] = 'You have joined this standup meeting group.'
+
+    component = ::StandupMeetingGroup::LeaveButtonComponent.new(standup_meeting_group: @standup_meeting_group,
+      current_user:, standup_meeting_group_user: @standup_meeting_group_user)
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.update(helpers.dom_id(@standup_meeting_group, :join_or_leave),
+          component)
+      end
+    end
   end
 
   def destroy
+    @standup_meeting_group = StandupMeetingGroup.find(params[:standup_meeting_group_id])
     @standup_meeting_group_user = StandupMeetingGroupUser.find(params[:id])
 
     authorize @standup_meeting_group_user, policy_class: StandupMeetingGroup::JoinPolicy
 
     @standup_meeting_group_user.destroy
-    flash[:notice] = 'You have left this standup meeting group.'
-    redirect_to standup_meeting_groups_path
+
+    component = ::StandupMeetingGroup::JoinButtonComponent.new(
+      standup_meeting_group: @standup_meeting_group, current_user:
+    )
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.update(
+          helpers.dom_id(@standup_meeting_group, :join_or_leave), component
+        )
+      end
+    end
   end
 end
