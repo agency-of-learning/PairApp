0b374d9b53c11d18cf14d8d097b329501f2af379
SHAMI_TOMITA <shamitomita@gmail.com>
2023-06-24 11:45:34 -0400

    javascript timezone converter (rails attempt next)

--- app/javascript/controllers/pair_requests_form_controller.js
+++ app/javascript/controllers/pair_requests_form_controller.js
@@ -2,28 +2,39 @@ import { Controller } from "@hotwired/stimulus"
 import { get } from "@rails/request.js"
 // Connects to data-controller="pair-requests-form"
 export default class extends Controller {
-  static targets = ["invitee", "userTime"]
+  static targets = ["invitee", "user", "inviteeTime", "userTime"]
   change(event) {
-    let invitee_id = event.target.value
-    console.log(invitee_id)
-    let invitee_time_zone = event.target.selectedOptions[0].dataset.timezone
-    console.log(invitee_time_zone)
-    //get(`/pair_requests/invitee?invitee_id=${invitee_id}`,{
-    //  responseKind: "turbo-stream"
-    //})
-    this.inviteeTarget.textContent = invitee_time_zone
-  }
-  time(event){
-    let date = event.target.value 
-    let user_time_zone = event.target.dataset.user
-      let time = new Date(date)
+    let invitee_id
+    let invitee_time_zone
+    let current_user_time_zone 
+    let time 
+    let userTime 
+    let inviteeTime
 
-    time = date.toLocaleString("en-US", {
-      timeZone: `${user_time_zone}`
-    });
-    console.log(time)
-    console.log(user_time_zone)
-    this.userTimeTarget.textContent = time
+    if(event.target.name == "pair_request[invitee_id]"){
+      invitee_id = event.target.selectedOptions[0].value
+      invitee_time_zone = event.target.selectedOptions[0].dataset.timezone
+      current_user_time_zone = event.target.selectedOptions[0].dataset.usertimezone
+      this.inviteeTarget.textContent = invitee_time_zone
+      this.userTarget.textContent = current_user_time_zone
+
+      this.inviteeTarget.value = invitee_time_zone
+      this.userTarget.value = current_user_time_zone
+     
+    }
+    else if(event.target.name == "pair_request[when]"){
+      time = new Date(event.target.value) 
+      userTime = time.toLocaleString("en-US", {
+        timeZone: this.userTarget.value
+      });
+      inviteeTime = time.toLocaleString("en-US", {
+        timeZone: this.inviteeTarget.value
+      });
+
+      this.userTimeTarget.textContent = userTime 
+      this.inviteeTimeTarget.textContent = inviteeTime
+    }
+     
   }
   
 }
