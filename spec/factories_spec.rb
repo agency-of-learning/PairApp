71623a15db5c9bf6057a636d76441a861c487060
Ariel Fogel <fogeltine@gmail.com>
2023-06-24 10:14:50 -0400

    fix the factories_spec

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'FactoryBot' do
   FactoryBot.factories.each do |factory|
+    # rubocop:disable RSpec/NoExpectationExample
     it "#{factory.name} should pass lint" do
-      expect(FactoryBot::Linter.new([factory]).lint!).not_to raise(Error)
+      FactoryBot::Linter.new([factory]).lint!
     end
+    # rubocop:enable RSpec/NoExpectationExample
   end
 end
