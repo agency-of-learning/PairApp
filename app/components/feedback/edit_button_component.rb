42bf5084e89b38f60470447e2a25d9ea54b5b37e
Josh Smith <jmsmith1018@gmail.com>
2023-06-28 10:04:25 -0700

    Refactor feedback's `EditButtonComponent` for more flexibility

--- app/components/feedback/edit_button_component.rb
+++ app/components/feedback/edit_button_component.rb
@@ -1,14 +1,32 @@
 # frozen_string_literal: true
 
 class Feedback::EditButtonComponent < ViewComponent::Base
-  def initialize(feedback:, current_user:)
+  STYLES = {
+    button: 'btn-primary',
+    link: 'btn-link btn-xs sm:btn-sm hover:no-underline'
+  }.freeze
+
+  def initialize(feedback:, current_user:, style: :button, classes: '')
     @feedback = feedback
     @current_user = current_user
+    @style = style
+    @classes = classes
+  end
+
+  def call
+    style_archetype = STYLES.fetch(style)
+
+    link_to(
+      edit_feedback_path(feedback),
+      class: "btn btn-sm capitalize #{style_archetype} #{classes}"
+    ) do
+      content || 'Edit'
+    end
   end
 
   private
 
-  attr_reader :feedback, :current_user
+  attr_reader :feedback, :current_user, :style, :classes
 
   def render?
     feedback.present? && Pundit.policy(current_user, feedback).edit?
