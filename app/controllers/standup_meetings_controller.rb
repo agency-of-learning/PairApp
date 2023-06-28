0d857ab3c907405201cffd5090c3faf55af4b1e1
Ariel Fogel <fogeltine@gmail.com>
2023-06-28 08:33:22 -0400

    restrict accessing the edit form unless you're authorized; gracefully handle unauthorized error

--- app/controllers/standup_meetings_controller.rb
+++ app/controllers/standup_meetings_controller.rb
@@ -3,13 +3,9 @@ class StandupMeetingsController < ApplicationController
 
   def edit
     @standup_meeting = StandupMeeting.find(params[:id])
-    # TODO: A naïve date range with a single min and max is probably fine for now,
-    # but there is an islands and gaps problem that should be handled to address
-    # issues when a user joins a group, leaves the group, and rejoins.
-    @date_range = [{
-      from: StandupMeeting.for_member(current_user, @standup_meeting_group).minimum(:meeting_date),
-      to: StandupMeeting.for_member(current_user, @standup_meeting_group).maximum(:meeting_date)
-    }]
+    authorize @standup_meeting
+  rescue Pundit::NotAuthorizedError
+    redirect_back_or_to(standup_meeting_groups_path)
   end
 
   private
