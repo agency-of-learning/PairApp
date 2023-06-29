73ce3223ae45e74ffef4410a5079d536a60a2826
Ariel Fogel <fogeltine@gmail.com>
2023-06-29 16:30:20 -0400

    enable switching between row and column views

--- app/components/standup_meeting/meeting_row_component.rb
+++ app/components/standup_meeting/meeting_row_component.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class StandupMeeting::MeetingRowComponent < ViewComponent::Base
+  with_collection_parameter :standup_meeting
+
   def initialize(standup_meeting:)
     @standup_meeting = standup_meeting
   end
