01fd548d9f52ff82d43590856816364b36f14b8b
SHAMI_TOMITA <shamitomita@gmail.com>
2023-07-04 13:40:31 -0400

    update

--- app/javascript/controllers/pair_requests_form_controller.js
+++ app/javascript/controllers/pair_requests_form_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import { get } from "@rails/request.js"
 // Connects to data-controller="pair-requests-form"
 export default class extends Controller {
-  static targets = ["click", "invitee", "user", "inviteeTime", "userTime"]
+  static targets = ["click", "invitee"]
   connect(){
     localStorage.clear()
     console.log("localStorage is cleared")
@@ -18,21 +18,18 @@ export default class extends Controller {
     //console.log("hi")
     console.log(event.target.selectedOptions[0].dataset)
     let invitee_time_zone = event.target.selectedOptions[0].dataset.timezone
-    let user_time_zone = event.target.selectedOptions[0].dataset.usertimezone
     
     localStorage.setItem("invitee_time_zone", invitee_time_zone)
-    localStorage.setItem("user_time_zone", user_time_zone)
-
-    let invitee_id = event.target.selectedOptions[0].value
 
     if(localStorage.getItem("pair_app_when")){
-      console.log("WHEN???", invitee_time_zone, user_time_zone)
-      const inviteeResult = new TimeZoneConverter(invitee_time_zone).conversion
-      const userResult = new TimeZoneConverter(user_time_zone).conversion
-     
       let time = localStorage.getItem("pair_app_when")
       time = new Date(time);
-      let inviteeTime = time.toLocaleString("en-US", {
+      console.log("WHEN???", invitee_time_zone)
+      const inviteeResult = new TimeZoneConverter(invitee_time_zone, time).conversion
+  
+     
+      
+   /*    let inviteeTime = time.toLocaleString("en-US", {
         timeZone: inviteeResult,
         year: 'numeric',
         month: 'numeric',
@@ -47,10 +44,8 @@ export default class extends Controller {
         day: 'numeric',
         hour: '2-digit',
         minute: '2-digit' 
-      });
-      console.log(time, inviteeTime, userTime)
-      this.userTimeTarget.textContent = (`${userTime}, ${user_time_zone}`)
-      this.inviteeTimeTarget.textContent = (`${inviteeTime}, ${invitee_time_zone}`)
+      }); */
+      this.inviteeTarget.textContent = (`${inviteeResult}, ${invitee_time_zone}`)
 
     }
     //get(`/pair_requests/invitee?invitee_id=${invitee_id}`,{
@@ -58,20 +53,19 @@ export default class extends Controller {
     //})
   }
 
+
+
   date(event){
     let time = new Date(event.target.value)
     localStorage.setItem("pair_app_when", time)
     if(localStorage.getItem("invitee_time_zone")){
       //console.log(localStorage.getItem("invitee_time_zone"))
       let inviteeValue = localStorage.getItem("invitee_time_zone")
-      let userValue = localStorage.getItem("user_time_zone")
       
-      const inviteeResult = new TimeZoneConverter(inviteeValue)
-      const userResult = new TimeZoneConverter(userValue)
+      const inviteeResult = new TimeZoneConverter(inviteeValue, time).conversion
 
-      console.log(inviteeResult, userResult)
       
-      let inviteeTime = time.toLocaleString("en-US", {
+/*       let inviteeTime = time.toLocaleString("en-US", {
         timeZone: inviteeResult.conversion,
         year: 'numeric',
         month: 'numeric',
@@ -87,10 +81,8 @@ export default class extends Controller {
         day: 'numeric',
         hour: '2-digit',
         minute: '2-digit' 
-      });
-      console.log(time, inviteeTime, userTime)
-      this.userTimeTarget.textContent = (`${userTime}, ${userValue}`)
-      this.inviteeTimeTarget.textContent = (`${inviteeTime}, ${inviteeValue}`)
+      }); */
+      this.inviteeTarget.textContent = (`${inviteeResult}, ${inviteeValue}`)
 
     }
   }
@@ -98,20 +90,28 @@ export default class extends Controller {
 }
 
 class TimeZoneConverter {
-  constructor(input) {
+  constructor(input, time) {
     this.input = input;
+    this.time = time
   }
 
   get conversion() {
+    let match
     let ary = Intl.supportedValuesOf('timeZone');
     if (this.input == 'UTC'){
-      console.log("UTC")
-      return this.input;
+      match = this.input
+    } else {
+      match = ary.find(element => element.match(this.input))
     }
-
-    let result = ary.find(element => element.match(this.input))
-    console.log(`Result: ${result}`)
-    return result 
+    const result = this.time.toLocaleString("en-US", {
+        timeZone: match,
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit' 
+      });
+      return result;
   }
 
 }
