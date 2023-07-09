5edd4291d95502d42ad9eb8bb4d195627e6d33c6
Ariel Fogel <fogeltine@gmail.com>
2023-07-09 14:07:27 +0300

    add index page for standup meetings

--- app/controllers/standup_meetings_controller.rb
+++ app/controllers/standup_meetings_controller.rb
@@ -1,7 +1,27 @@
 class StandupMeetingsController < ApplicationController
+  # rubocop:disable Metrics/AbcSize
+  def index
+    @meeting_date = params[:date].nil? ? Date.current : Date.parse(params[:date])
+    @standup_meetings = StandupMeeting.includes(:user, :standup_meeting_group)
+                                      .where(
+                                        meeting_date: @meeting_date,
+                                        standup_meeting_group_id: params[:standup_meeting_group_id]
+                                      )
+    @date_options = StandupMeeting.meeting_dates(params[:standup_meeting_group_id])
+    if @standup_meetings.empty?
+      flash[:alert] = 'There are no standups for the date you selected'
+      redirect_to standup_meeting_group_standup_meetings_path(params[:standup_meeting_group_id],
+        date: @date_options.max)
+    else
+      @completed_meetings = @standup_meetings.filter(&:completed?)
+      @current_user_standup_meeting = @standup_meetings.find { |meet| meet.user == current_user }
+      @standup_meeting_group = @standup_meetings.first.standup_meeting_group
+    end
+  end
+  # rubocop:enable Metrics/AbcSize
+
   def edit
-    @standup_meeting = StandupMeeting.includes(:standup_meeting_group)
-                                     .find(params[:id])
+    @standup_meeting = StandupMeeting.includes(:standup_meeting_group).find(params[:id])
     @standup_meeting_group = @standup_meeting.standup_meeting_group
     authorize @standup_meeting
   end
