dd79f0f383160e772844556f387b420e46882934
Ariel Fogel <fogeltine@gmail.com>
2023-07-10 19:33:10 +0300

    add minimum date for flatpickr, make stimulus controller more generalizable

--- app/javascript/controllers/link_builder_controller.js
+++ app/javascript/controllers/link_builder_controller.js
@@ -3,12 +3,15 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="link-builder"
 export default class extends Controller {
   static targets = ['source', 'destination']
-  static values = { param: String }
+  static values = { 
+    param: String, 
+    url: String
+  }
 
   updateDestinationParams() {
     const url = new URL(this.destinationTarget.href);
     const params = new URLSearchParams(url.search);
     params.set(this.paramValue, this.sourceTarget.value)
-    this.destinationTarget.href = `${url.origin}${url.pathname}?${params}`;
+    this.destinationTarget.href = `${this.urlValue}?${params}`;
   }
 }
