33d359627006159a7a184816deec3dab10e77c52
Ariel Fogel <fogeltine@gmail.com>
2023-07-10 19:39:49 +0300

    add index page for standup meetings (#145)
    * add index page for standup meetings
    
    * remove instance variables that are not relevant to current PR
    
    * remove linter disable
    
    * simplify standupmeeting controller
    
    * add authorization to index page
    
    * fix the tests
    
    * fix nit
    
    * update query to use `exists?`

--- app/controllers/standup_meetings_controller.rb
+++ app/controllers/standup_meetings_controller.rb
@@ -1,7 +1,16 @@
 class StandupMeetingsController < ApplicationController
+  def index
+    @meeting_date = params[:date].nil? ? Date.current : Date.parse(params[:date])
+    @standup_meeting_group = StandupMeetingGroup.includes(:users)
+                                                .find(params[:standup_meeting_group_id])
+    authorize @standup_meeting_group, policy_class: StandupMeetingPolicy
+    @standup_meetings = @standup_meeting_group.standup_meetings
+                                              .includes(:user)
+                                              .where(meeting_date: @meeting_date)
+  end
+
   def edit
-    @standup_meeting = StandupMeeting.includes(:standup_meeting_group)
-                                     .find(params[:id])
+    @standup_meeting = StandupMeeting.includes(:standup_meeting_group).find(params[:id])
     @standup_meeting_group = @standup_meeting.standup_meeting_group
     authorize @standup_meeting
   end
