cbecd3118c9693354c1c4e1a8c024cdbdc838cbc
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-07-10 13:02:44 -0700

    Define @standup_meeting_group_user variable in joined_card_component.rb co-author: Roc Devenport rubykod@gmail.com

--- app/components/standup_meeting_group/joined_card_component.rb
+++ app/components/standup_meeting_group/joined_card_component.rb
@@ -8,5 +8,6 @@ class StandupMeetingGroup::JoinedCardComponent < ViewComponent::Base
   def initialize(standup_meeting_group:, current_user:)
     @standup_meeting_group = standup_meeting_group
     @user = current_user
+    @standup_meeting_group_user = standup_meeting_group.standup_meeting_groups_users.find_by(user: user)
   end
 end
