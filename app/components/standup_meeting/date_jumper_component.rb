033481e9fcd07dfbb69e705d0fa8f7c183d3ecb8
Ariel Fogel <fogeltine@gmail.com>
2023-07-12 15:19:09 +0300

    extract date jump component, update format for jumping dates

---
+++ app/components/standup_meeting/date_jumper_component.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class StandupMeeting::DateJumperComponent < ViewComponent::Base
+  def initialize(standup_meeting_group:, meeting_date:)
+    @standup_meeting_group = standup_meeting_group
+    @meeting_date = meeting_date
+  end
+
+  private
+  attr_reader :standup_meeting_group
+
+  def meeting_date
+    @meeting_date.to_date.strftime('%m-%d-%Y')
+  end
+
+  def base_url
+    standup_meeting_group_standup_meetings_path(standup_meeting_group)
+  end
+
+  def default_url
+    base_url + "?date=#{meeting_date}"
+  end
+end
--- app/components/standup_meeting/date_navigator_component.rb
+++ app/components/standup_meeting/date_navigator_component.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 class StandupMeeting::DateNavigatorComponent < ViewComponent::Base
-  def initialize(standup_meeting_group:, meeting_date: Date.current)
+  def initialize(standup_meeting_group:, meeting_date:)
     @standup_meeting_group = standup_meeting_group
     @meeting_date = meeting_date
   end
 
   private
 
-  attr_reader :standup_meeting_group, :meeting_date, :date_options
+  attr_reader :standup_meeting_group, :date_options, :meeting_date
 
   def next_standup_meeting
-    meeting_date.tomorrow
+    meeting_date.tomorrow.strftime('%m-%d-%Y')
   end
 
   def next_standup_meeting?
@@ -19,7 +19,7 @@ class StandupMeeting::DateNavigatorComponent < ViewComponent::Base
   end
 
   def previous_standup_meeting
-    meeting_date.yesterday
+    meeting_date.yesterday.strftime('%m-%d-%Y')
   end
 
   def previous_standup_meeting?
