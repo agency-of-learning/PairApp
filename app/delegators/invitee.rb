6b3183b5d7acd5460d452b165e9f3bb6f63320cc
SHAMI_TOMITA <shamitomita@gmail.com>
2023-07-14 11:35:21 -0400

    rubocop

--- app/delegators/invitee.rb
+++ app/delegators/invitee.rb
@@ -1,5 +1,5 @@
 class Invitee < DelegateClass(User)
-    def time_zone_identifier 
-         ActiveSupport::TimeZone.new(time_zone).tzinfo.identifier
-    end 
-end 
+  def time_zone_identifier
+    ActiveSupport::TimeZone.new(time_zone).tzinfo.identifier
+  end
+end
