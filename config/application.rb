28680a4ca78e00c40a284189bbc82567e40d5db9
Josh Smith <jmsmith1018@gmail.com>
2023-07-18 09:39:00 -0700

    Split workers from jobs (#172)
    * Move `AutoExpireJob` to `AutoExpireWorker`
    
    * Configure ActiveJob
    
    * Prevent currently enqueued AutoExpireJobs from failing
    
    This can be removed when all of the old jobs are gone
    
    * Fix namespacing of old job

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module PairApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_storage.variant_processor = :mini_magick
+    config.active_job.queue_adapter = :sidekiq
   end
 end
