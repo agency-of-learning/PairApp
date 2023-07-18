30963009a722d18398a98da0742e89a54cce0d59
Josh Smith <jmsmith1018@gmail.com>
2023-07-17 22:16:47 -0700

    Prevent currently enqueued AutoExpireJobs from failing
    This can be removed when all of the old jobs are gone

--- app/workers/pair_request/auto_expire_worker.rb
+++ app/workers/pair_request/auto_expire_worker.rb
@@ -10,3 +10,6 @@ class PairRequest
     # rubocop:enable Rails/SkipsModelValidations
   end
 end
+
+# TODO: delete this when all old `PairRequest::AutoExpireJob`s have dequeued
+PairRequest::AutoExpireJob = PairRequest::AutoExpireWorker
