6520e32da9afcd7b2909cfc01bb953da09f7c5c5
Josh Smith <jmsmith1018@gmail.com>
2023-07-19 17:23:17 -0700

    Deliver completion reminder notification after acceptance

--- app/controllers/pair_requests/acceptances_controller.rb
+++ app/controllers/pair_requests/acceptances_controller.rb
@@ -3,9 +3,16 @@ class PairRequests::AcceptancesController < ApplicationController
     @pair_request = PairRequest.find(params[:pair_request_id])
     authorize @pair_request, policy_class: PairRequest::AcceptancePolicy
     @pair_request.accepted!
-    PairRequest::AcceptanceNotification.with(pair_request: @pair_request).deliver(@pair_request.author)
+    send_notifications
 
     flash[:notice] = 'You have accepted this pair request.'
     redirect_to pair_request_path(@pair_request)
+  end
+
+  private
+
+  def send_notifications
+    PairRequest::AcceptanceNotification.with(pair_request: @pair_request).deliver(@pair_request.author)
+    PairRequest::CompletionReminderNotification.with(pair_request: @pair_request).deliver(@pair_request.author)
   end
 end
