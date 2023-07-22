ca555981907221d191b90040a1123713531bc2a4
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-07-21 19:04:59 -0700

    Adds destroy.turbo_stream.erb file and updates controller to render turbo stream on destroy co-authored-by: RocDavenport rubykod@gmail.com

--- app/controllers/standup_meeting_groups/joins_controller.rb
+++ app/controllers/standup_meeting_groups/joins_controller.rb
@@ -22,16 +22,9 @@ class StandupMeetingGroups::JoinsController < ApplicationController
     authorize @standup_meeting_group_user, policy_class: StandupMeetingGroup::JoinPolicy
 
     @standup_meeting_group_user.destroy
-
-    component = ::StandupMeetingGroup::JoinButtonComponent.new(
-      standup_meeting_group: @standup_meeting_group, current_user:
-    )
-
     respond_to do |format|
       format.turbo_stream do
-        render turbo_stream: turbo_stream.update(
-          helpers.dom_id(@standup_meeting_group, :join_or_leave), component
-        )
+        render 'standup_meeting_groups/joins/destroy'
       end
     end
   end
