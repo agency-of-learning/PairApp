f5ec015c35acc37864baff46e04b73e5e37c71d6
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-07-22 15:01:59 -0700

    wip: adding joinable_item_component specs

---
+++ spec/components/standup_meeting_group/joinable_item_component_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper' 
+
+RSpec.describe StandupMeetingGroup::JoinableItemComponent, type: :component do
+  let(:standup_meeting_group) { create(:standup_meeting_group)}
+  let(:user) { create(:user) }
+  context 'When a standup meeting group is present and the user is not a member' do
+    it 'renders a join button' do
+      render_inline(
+        described_class.new(standup_meeting_group: standup_meeting_group, current_user: user)
+      )
+      expect(page).to have_button('Join')
+    end
+  end
+
+  context 'When a standup meeting group is present and the user is a member' do
+    it 'renders a leave button' do
+      standup_meeting_group.users << user
+
+      render_inline(
+        described_class.new(standup_meeting_group: standup_meeting_group, current_user: user)
+      )
+      binding.pry
+      expect(page).not_to have_button('Join')
+    end
+  end
+end 
