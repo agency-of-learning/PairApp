5f2702749110bbd584959e16e080b89d32ebb654
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-07-29 15:21:51 -0700

    fixes rubocop errors

--- spec/components/standup_meeting_group/joinable_item_component_spec.rb
+++ spec/components/standup_meeting_group/joinable_item_component_spec.rb
@@ -5,12 +5,12 @@ require 'rails_helper'
 RSpec.describe StandupMeetingGroup::JoinableItemComponent, type: :component do
   let(:standup_meeting_group) { create(:standup_meeting_group) }
   let(:user) { create(:user) }
-  
+
   context 'when standup meeting group is present and user is not a member' do
     it 'renders the join button' do
-      render_inline(described_class.new(standup_meeting_group: standup_meeting_group, current_user: user))
+      render_inline(described_class.new(standup_meeting_group:, current_user: user))
 
-      expect(page).to have_css('button', text: 'Join')
+      expect(page).to have_button(text: 'Join')
     end
   end
 end
