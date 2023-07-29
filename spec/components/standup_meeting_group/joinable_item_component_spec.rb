498b5f480c31a07d847ec392ae3796f04633ce22
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-07-29 14:48:03 -0700

    Adds test for joinableItemComponent

--- spec/components/standup_meeting_group/joinable_item_component_spec.rb
+++ spec/components/standup_meeting_group/joinable_item_component_spec.rb
@@ -5,24 +5,12 @@ require 'rails_helper'
 RSpec.describe StandupMeetingGroup::JoinableItemComponent, type: :component do
   let(:standup_meeting_group) { create(:standup_meeting_group) }
   let(:user) { create(:user) }
-
+  
   context 'when standup meeting group is present and user is not a member' do
-    it 'renders a join button' do
-      render_inline(
-        described_class.new(standup_meeting_group:, current_user: user)
-      )
-      expect(page).to have_button('Join')
+    it 'renders the join button' do
+      render_inline(described_class.new(standup_meeting_group: standup_meeting_group, current_user: user))
+
+      expect(page).to have_css('button', text: 'Join')
     end
   end
-
-  # context 'When a standup meeting group is present and the user is a member' do
-  #   it 'renders a leave button' do
-  #     standup_meeting_group.users << user
-
-  #     # render_inline(
-  #     #   described_class.new(standup_meeting_group: standup_meeting_group, current_user: user)
-  #     # )
-  #     # expect(page).not_to have_button('Join')
-  #   end
-  # end
 end
