8b767a120b610a126949e634985156f0971e5f60
Daniel Rassiner <daniel@rassiner.com>
2023-07-31 21:07:52 -0700

    Standup meeting group refactor (#138)
    * wip
    
    * wip
    
    * Define @standup_meeting_group_user variable in
    joined_card_component.rb
    co-author: Roc Devenport rubykod@gmail.com
    
    * Add standup_meeting_groups::joins_controller.rb
    destroy action
    co-author: Roc Devenport rubykod@gmail.com
    
    * Defines @standup_meeting_group_user variable in
    joined_card_component.html.erb
    co-authored-by: Roc Devenport rubykod@gmail.com
    
    * .
    
    * Adds destroy.turbo_stream.erb file and updates
    controller to render turbo stream on destroy
    co-authored-by: RocDavenport rubykod@gmail.com
    
    * fixes linting error
    
    * breaks @joinable_standup_meeting_groups line the standup_meeting_groups_controller.rb
    
    * .
    
    * .
    
    * wip: adding joinable_item_component specs
    
    * removes join_button_component
    
    * removes leave_button_component
    
    * removed base_botton_component
    
    * Remove uneeded code from
    _standup_meeting_group_list_item.erb
    
    * adds styling changes to joined_card_component.html.erb
    
    * Refactores standup meeting group index to only
    display 3 cards per row.
    
    * clean code
    
    * Address pr comments
    
    * centered joined_card_components on small screen
    
    * deleted commented out code
    
    * wip: adding checkin_status feature to joined_cards
    
    * reverting to split up issues
    
    * adds icons and flash text for checkins messages
    
    * cleans code for pr
    
    * Adds test for joinableItemComponent
    
    * adds joined card component test
    
    * fixes rubocop errors
    
    * removes turbo from leave button and adds a redirect to joins destroy action
    
    * Changes joined card components container from
    grid to flexbox
    
    * refactor standup_meeting_groups turbo_stream
    to render JoinableItemComponent
    
    * remove _standup_meeting_group_list item partial
    
    ---------
    
    Co-authored-by: Gustavo Valenzuela <gustavo.devvv@gmail.com>

---
+++ spec/components/standup_meeting_group/joinable_item_component_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe StandupMeetingGroup::JoinableItemComponent, type: :component do
+  let(:standup_meeting_group) { create(:standup_meeting_group) }
+  let(:user) { create(:user) }
+
+  context 'when standup meeting group is present and user is not a member' do
+    it 'renders the join button' do
+      render_inline(described_class.new(standup_meeting_group:, current_user: user))
+
+      expect(page).to have_button(text: 'Join')
+    end
+  end
+end
