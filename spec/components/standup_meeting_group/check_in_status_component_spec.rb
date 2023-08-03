55940cf11e0a834e66f64c0db7a8777d39f36dd8
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-08-03 15:31:38 -0700

    Completes check_in_status_component_spec

--- spec/components/standup_meeting_group/check_in_status_component_spec.rb
+++ spec/components/standup_meeting_group/check_in_status_component_spec.rb
@@ -1,22 +1,27 @@
 # frozen_string_literal: true
 
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe StandupMeetingGroup::CheckInStatusComponent, type: :component do
-  let(:standup_meeting_group) { create(:standup_meeting_group) }
-  let(:standup_meeting_group_2) { create(:standup_meeting_group) }
   let(:user) { create(:user) }
-  let(:standup_meeting) { create(:standup_meeting, user: user, standup_meeting_group: standup_meeting_group, status: 'completed') }
+
+  let(:standup_meeting_group) do
+    create(:standup_meeting_group).tap do |group|
+      group.standup_meetings << create(:standup_meeting, user: user, status: 'completed')
+    end
+  end
+  let(:standup_meeting_group2) { create(:standup_meeting_group) }
+
 
   it "renders 'Checked in' message when user has checked in" do
     render_inline(described_class.new(standup_meeting_group, user))
 
-    expect(page).to have_css("p", text: "Checked in")
+    expect(page).to have_css('p', text: 'Checked in')
   end
 
   it "renders 'Outstanding check-in' message when user has not checked in" do
-    render_inline(described_class.new(standup_meeting_group_2, user))
+    render_inline(described_class.new(standup_meeting_group2, user))
 
-    expect(page).to have_css("p", text: "Outstanding check-in")
+    expect(page).to have_css('p', text: 'Outstanding check-in')
   end
 end
