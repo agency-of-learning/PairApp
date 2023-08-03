da7e5363bc6de0203d3d28a09dc8f6e5e375a60a
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-08-03 13:51:30 -0700

    Adds check_in_status_component specs

--- spec/components/standup_meeting_group/check_in_status_component_spec.rb
+++ spec/components/standup_meeting_group/check_in_status_component_spec.rb
@@ -3,13 +3,20 @@
 require "rails_helper"
 
 RSpec.describe StandupMeetingGroup::CheckInStatusComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:standup_meeting_group) { create(:standup_meeting_group) }
+  let(:standup_meeting_group_2) { create(:standup_meeting_group) }
+  let(:user) { create(:user) }
+  let(:standup_meeting) { create(:standup_meeting, user: user, standup_meeting_group: standup_meeting_group, status: 'completed') }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it "renders 'Checked in' message when user has checked in" do
+    render_inline(described_class.new(standup_meeting_group, user))
+
+    expect(page).to have_css("p", text: "Checked in")
+  end
+
+  it "renders 'Outstanding check-in' message when user has not checked in" do
+    render_inline(described_class.new(standup_meeting_group_2, user))
+
+    expect(page).to have_css("p", text: "Outstanding check-in")
+  end
 end
