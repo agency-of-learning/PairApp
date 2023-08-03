15fd65ca56e0e8105228179ad2d1400f40c52c8c
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-08-03 15:41:25 -0700

    rubocop cleanup

--- spec/components/standup_meeting_group/check_in_status_component_spec.rb
+++ spec/components/standup_meeting_group/check_in_status_component_spec.rb
@@ -7,21 +7,20 @@ RSpec.describe StandupMeetingGroup::CheckInStatusComponent, type: :component do
 
   let(:standup_meeting_group) do
     create(:standup_meeting_group).tap do |group|
-      group.standup_meetings << create(:standup_meeting, user: user, status: 'completed')
+      group.standup_meetings << create(:standup_meeting, user:, status: 'completed')
     end
   end
   let(:standup_meeting_group2) { create(:standup_meeting_group) }
 
-
   it "renders 'Checked in' message when user has checked in" do
     render_inline(described_class.new(standup_meeting_group, user))
 
-    expect(page).to have_content("Checked in")
+    expect(page).to have_content('Checked in')
   end
 
   it "renders 'Outstanding check-in' message when user has not checked in" do
     render_inline(described_class.new(standup_meeting_group2, user))
 
-    expect(page).to have_content("Outstanding check-in")
+    expect(page).to have_content('Outstanding check-in')
   end
 end
