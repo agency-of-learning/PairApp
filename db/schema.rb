0194605a32375b748bd3a1cf292476c0c2e68db0
Roc Davenport <rubykod@gmail.com>
2023-08-05 23:00:53 -0400

    updating branch from main

--- db/schema.rb
+++ db/schema.rb
@@ -98,6 +98,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_31_005819) do
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
+  create_table "standup_attendances", force: :cascade do |t|
+    t.bigint "standup_meeting_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["standup_meeting_id"], name: "index_standup_attendances_on_standup_meeting_id"
+    t.index ["user_id"], name: "index_standup_attendances_on_user_id"
+  end
+
   create_table "standup_meeting_groups", force: :cascade do |t|
     t.string "name", null: false
     t.boolean "active", default: true, null: false
@@ -175,6 +184,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_31_005819) do
   add_foreign_key "pair_requests", "users", column: "author_id"
   add_foreign_key "pair_requests", "users", column: "invitee_id"
   add_foreign_key "profiles", "users"
+  add_foreign_key "standup_attendances", "standup_meetings"
+  add_foreign_key "standup_attendances", "users"
   add_foreign_key "standup_meetings", "standup_meeting_groups"
   add_foreign_key "standup_meetings", "users"
 end
