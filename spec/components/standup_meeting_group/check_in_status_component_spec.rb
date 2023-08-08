4145a2aba165edfa4073bdb3f4a0bc7b1ba6f74c
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-08-07 19:37:40 -0700

    changes variable names in check_in_status_component_spec

--- spec/components/standup_meeting_group/check_in_status_component_spec.rb
+++ spec/components/standup_meeting_group/check_in_status_component_spec.rb
@@ -5,21 +5,22 @@ require 'rails_helper'
 RSpec.describe StandupMeetingGroup::CheckInStatusComponent, type: :component do
   let(:user) { create(:user) }
 
-  let(:standup_meeting_group) do
+  let(:standup_meeting_group_with_checkin) do
     create(:standup_meeting_group).tap do |group|
       group.standup_meetings << create(:standup_meeting, user:, status: 'completed')
     end
   end
-  let(:standup_meeting_group2) { create(:standup_meeting_group) }
+
+  let(:standup_meeting_group_no_checkin) { create(:standup_meeting_group) }
 
   it "renders 'Checked in' message when user has checked in" do
-    render_inline(described_class.new(standup_meeting_group, user))
+    render_inline(described_class.new(standup_meeting_group_with_checkin, user))
 
     expect(page).to have_content('Checked in')
   end
 
   it "renders 'Outstanding check-in' message when user has not checked in" do
-    render_inline(described_class.new(standup_meeting_group2, user))
+    render_inline(described_class.new(standup_meeting_group_no_checkin, user))
 
     expect(page).to have_content('Outstanding check-in')
   end
