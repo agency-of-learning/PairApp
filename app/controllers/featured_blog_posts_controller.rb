c1a95a9e65283751c95e9d0b3d79d9ae4b34281e
Josh Smith <jmsmith1018@gmail.com>
2023-08-15 14:48:09 -0700

    Add routes and controller for featured blog

---
+++ app/controllers/featured_blog_posts_controller.rb
@@ -0,0 +1,27 @@
+class FeaturedBlogPostsController < ApplicationController
+  def index
+    @featured_blog_posts = BlogPost.all_featured
+  end
+
+  def create
+    featured_blog_post = FeaturedBlogPost.new(featured_blog_post_params)
+    authorize featured_blog_post
+    featured_blog_post.save!
+
+    redirect_to featured_blog_posts_path, success: 'Post added to Featured Blog!'
+  end
+
+  def destroy
+    featured_blog_post = FeaturedBlogPost.find(params[:id])
+    authorize featured_blog_post
+    featured_blog_post.destroy!
+
+    redirect_to featured_blog_posts_path, notice: 'Post removed from Featured Blog!'
+  end
+
+  private
+
+  def featured_blog_post_params
+    params.require(:featured_blog_post).permit(:blog_post_id)
+  end
+end
