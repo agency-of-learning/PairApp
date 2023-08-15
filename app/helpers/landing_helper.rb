e59da5371a59dcc738fab68111b0d2df816262bf
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-08-15 11:27:26 -0700

    Abstracts out CTA's buttons to a method helper cta_button

--- app/helpers/landing_helper.rb
+++ app/helpers/landing_helper.rb
@@ -1,2 +1,8 @@
 module LandingHelper
+  def cta_button(text, icon_class)
+    content_tag(:button, class: "flex justify-between items-center w-full h-[70px] mb-4 bg-[#38D68E] text-[#363942] font-bold px-4 rounded-lg hover:scale-105 transform transition-transform") do
+      concat(text)
+      concat(content_tag(:i, "", class: "fa-solid fa-arrow-right fa-lg color-[#363942] #{icon_class}"))
+    end
+  end
 end
