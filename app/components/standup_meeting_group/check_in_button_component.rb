63b3a799c47f9fdb0f3790029e31189941b9bab1
Roc Davenport <rubykod@gmail.com>
2023-08-18 03:31:01 -0400

    cleanup rubocop concerns

--- app/components/standup_meeting_group/check_in_button_component.rb
+++ app/components/standup_meeting_group/check_in_button_component.rb
@@ -1,5 +1,6 @@
 class StandupMeetingGroup::CheckInButtonComponent < ViewComponent::Base
   def initialize(standup_meeting_group)
+    super
     @standup_meeting_group = standup_meeting_group
   end
 end
