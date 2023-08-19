fa456ea957a6aed26d0b7454e42b9b62f536fa10
Josh Smith <jmsmith1018@gmail.com>
2023-08-19 10:33:24 -0700

    Implement NullObject pattern for users

---
+++ app/models/null/user.rb
@@ -0,0 +1,17 @@
+class Null::User
+  def admin?
+    false
+  end
+
+  def member?
+    false
+  end
+
+  def present?
+    false
+  end
+
+  def blank?
+    true
+  end
+end
--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -4,7 +4,7 @@ class ApplicationPolicy
   attr_reader :user, :record
 
   def initialize(user, record)
-    @user = user
+    @user = user || Null::User.new
     @record = record
   end
 
