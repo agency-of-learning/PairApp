d48130948d4080a67cb0ad77c901818dd649120c
Josh Smith <jmsmith1018@gmail.com>
2023-08-20 13:20:02 -0700

    Friendly Routing for Blog Posts (#247)
    * Add friendly id slugging to blog posts
    
    * Implement NullObject pattern for users
    
    * Fix bug with associated featured blog post deletion
    
    * Remove authentication from `blog_posts#show`
    
    * Add tests for behavior of friendly blog post slugs
    
    * Fix policy spec for featured blog posts
    
    * Correct spec name for blog post request specs
    
    * Fix null user spec file name
    
    * Remove null user
    
    * Alter policy checks for creating a featured post

--- spec/policies/featured_blog_post_policy_spec.rb
+++ spec/policies/featured_blog_post_policy_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe FeaturedBlogPostPolicy, type: :policy do
 
   let(:user) { create(:user) }
   let(:admin) { create(:user, :admin) }
-  let(:featured_published_post) { create(:featured_blog_post, :with_published_post) }
+  let(:featured_published_post) { build(:featured_blog_post, :with_published_post) }
 
   permissions :update?, :destroy? do
     it 'denies permission for a regular user' do
@@ -14,30 +14,6 @@ RSpec.describe FeaturedBlogPostPolicy, type: :policy do
 
     it 'grants permission for an admin' do
       expect(subject).to permit(admin, featured_published_post)
-    end
-  end
-
-  permissions :create? do
-    context 'when the featured blog is published' do
-      it 'grants permission for an admin' do
-        expect(subject).to permit(admin, featured_published_post)
-      end
-
-      it 'denies permission to a regular user' do
-        expect(subject).not_to permit(user, featured_published_post)
-      end
-    end
-
-    context 'when the featured post is in a draft state' do
-      let(:featured_draft_post) { create(:featured_blog_post) }
-
-      it 'denies permission to a regular user' do
-        expect(subject).not_to permit(user, featured_draft_post)
-      end
-
-      it 'denies permission to an admin user' do
-        expect(subject).not_to permit(admin, featured_draft_post)
-      end
     end
   end
 
