2aa158a8209b9d41024e6783cb3b0d3b13c1ffd8
Josh Smith <jmsmith1018@gmail.com>
2023-08-22 10:34:48 -0700

    Remove component for check-in button

--- app/components/standup_meeting_group/check_in_button_component.rb
+++
@@ -1,6 +0,0 @@
-class StandupMeetingGroup::CheckInButtonComponent < ViewComponent::Base
-  def initialize(standup_meeting_group)
-    super
-    @standup_meeting_group = standup_meeting_group
-  end
-end
