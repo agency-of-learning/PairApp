312867bcbfb025824bcd7a2fe61eafef0b1ac7d9
Josh Smith <jmsmith1018@gmail.com>
2023-08-22 09:37:21 -0700

    Fix tests for check_in_status_component
    Co-authored-by: Roc Davenport <rubykod@gmail.com>

--- spec/components/standup_meeting/button_group_component.rb
+++ spec/components/standup_meeting/button_group_component.rb
@@ -3,13 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe StandupMeeting::ButtonGroupComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:standup_meeting_group) { create(:standup_meeting_group) }
+  let(:standup_meeting) { create(:standup_meeting, status:, user:) }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  context 'when the standup meeting is a draft' do
+    let(:status) { :draft }
+
+    it 'renders a Check-In link' do
+      render_inline(described_class.new(standup_meeting_group:, standup_meeting:, current_user: user))
+
+      expect(page).to have_link('Check in')
+    end
+
+    it 'renders a Skip button' do
+      render_inline(described_class.new(standup_meeting_group:, standup_meeting:, current_user: user))
+
+      expect(page).to have_button('Skip')
+    end
+  end
+
+  context 'when the standup meeting is completed' do
+    let(:status) { :completed }
+
+    it 'renders an Edit Response link' do
+      render_inline(described_class.new(standup_meeting_group:, standup_meeting:, current_user: user))
+
+      expect(page).to have_link('Edit Response')
+    end
+  end
 end
