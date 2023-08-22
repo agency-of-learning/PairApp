072aeeefe0ad74043e78b64f9a2b6591693cceb1
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-08-22 13:39:50 -0700

    Adds Rejections controller

---
+++ app/controllers/user_mentee_applications/rejections_controller.rb
@@ -0,0 +1,9 @@
+class UserMenteeApplications::RejectionsController < ApplicationController
+  
+  def create
+    @user_mentee_application = UserMenteeApplication.find(params[:user_mentee_application_id])
+    @user_mentee_application.rejected!
+    @user_mentee_application.status_changed_by_id = current_user.id
+  end
+
+end
