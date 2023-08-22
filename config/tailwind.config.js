1cb6145b8d5589c799fffc835417841980467477
Ariel Fogel <fogeltine@gmail.com>
2023-08-22 15:28:17 +0300

    style consistency across application

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,32 +1,44 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}',
-    './app/components/**/*.{rb,erb,html}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
+    "./app/components/**/*.{rb,erb,html}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
   },
   daisyui: {
-    themes: ["light"]
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          primary: "#38d68e",
+          "primary-focus": "#03985c",
+          "primary-content": "#ecfdf3",
+          neutral: "#363942",
+          "neutral-focus": "#2C2E36",
+          "neutral-content": "#ededf1",
+          secondary: "#45aeeb",
+          "secondary-focus": "#0e5e96",
+          "secondary-content": "#f1f8fe",
+        },
+      },
+    ],
   },
   plugins: [
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
     require("daisyui"),
   ],
   purge: {
-    safelist: [
-      'alert-error',
-      'alert-success',
-    ]
-  }
-}
+    safelist: ["alert-error", "alert-success"],
+  },
+};
