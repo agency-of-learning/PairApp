9664b612485a45feb04f78c03a2241d881fe31ba
Josh Smith <jmsmith1018@gmail.com>
2023-08-22 09:08:43 -0700

    Make check in status more generic for display on index + show
    Co-authored-by: Roc Davenport <rubykod@gmail.com>

--- app/components/standup_meeting_group/check_in_status_component.rb
+++ app/components/standup_meeting_group/check_in_status_component.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 class StandupMeetingGroup::CheckInStatusComponent < ViewComponent::Base
-  def initialize(standup_meeting_group, current_user)
+  def initialize(standup_meeting_group:, current_user:, class_names: '')
     @standup_meeting_group = standup_meeting_group
     @user = current_user
     @standup_meeting = @standup_meeting_group.standup_meetings.find_by(user_id: @user.id, meeting_date: Date.current)
+    @class_names = class_names
   end
+
+  attr_reader :class_names
 end
