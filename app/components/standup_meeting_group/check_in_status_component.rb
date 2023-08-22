99ebe20aaa7466476a03d069a87a6ba3cba09d45
Josh Smith <jmsmith1018@gmail.com>
2023-08-22 10:24:21 -0700

    Add in style variants for check in status
    Co-authored-by: Roc Davenport <rubykod@gmail.com>

--- app/components/standup_meeting_group/check_in_status_component.rb
+++ app/components/standup_meeting_group/check_in_status_component.rb
@@ -1,12 +1,32 @@
 # frozen_string_literal: true
 
 class StandupMeetingGroup::CheckInStatusComponent < ViewComponent::Base
-  def initialize(standup_meeting_group:, current_user:, class_names: '')
+  ICON_STYLES = {
+    small: 'mr-1',
+    large: 'mr-4'
+  }.freeze
+
+  TEXT_STYLES = {
+    small: 'text-sm',
+    large: 'text-xl'
+  }.freeze
+
+  def initialize(standup_meeting_group:, current_user:, variant: :small)
     @standup_meeting_group = standup_meeting_group
     @user = current_user
     @standup_meeting = @standup_meeting_group.standup_meetings.find_by(user_id: @user.id, meeting_date: Date.current)
-    @class_names = class_names
+    @variant = variant
   end
 
-  attr_reader :class_names
+  attr_reader :variant
+
+  private
+
+  def p_tag_classes
+    TEXT_STYLES.fetch(variant)
+  end
+
+  def icon_classes
+    ICON_STYLES.fetch(variant)
+  end
 end
