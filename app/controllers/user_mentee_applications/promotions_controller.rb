05443c70c58acc847791bdb4b501d187fe8b1876
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-08-22 19:16:09 -0700

    Cleans promotions controller

--- app/controllers/user_mentee_applications/promotions_controller.rb
+++ app/controllers/user_mentee_applications/promotions_controller.rb
@@ -1,16 +1,14 @@
 class UserMenteeApplications::PromotionsController < ApplicationController
-
-
   def create 
     @user_mentee_application = UserMenteeApplication.find(params[:user_mentee_application_id])
-    @mentee_application_state = @user_mentee_application.mentee_application_state.new(mentee_application_state_params)
+    @mentee_application_state = @user_mentee_application.mentee_application_states.new(status: "Nexted", status_changed_by_id: current_user.id)
+
+
+   if @mentee_application_state.save
+    # @mentee_application_state.status = "Nexted"
+    redirect_to user_mentee_applications_path, notice: 'Successfully promoted.'
+   else
+    render some_error_path, alert: 'Failed to promote.'
+   end
   end
-
-
-  private
-
-  def mentee_application_state_params
-    params.require(:mentee_application_state).permit(:status, :notes, :user_id, :user_mentee_application_id, :status_changed_by_id)
-  end
-  
 end
