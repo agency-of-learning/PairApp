c7ccb15077c06e881661e09311cc36a42b2478a9
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-08-24 17:04:05 -0700

    Refactors create action to use promote_application helper

--- app/controllers/user_mentee_applications/promotions_controller.rb
+++ app/controllers/user_mentee_applications/promotions_controller.rb
@@ -1,14 +1,11 @@
 class UserMenteeApplications::PromotionsController < ApplicationController
   def create 
     @user_mentee_application = UserMenteeApplication.find(params[:user_mentee_application_id])
-    @mentee_application_state = @user_mentee_application.mentee_application_states.new(status: "Nexted", status_changed_by_id: current_user.id)
 
-
-   if @mentee_application_state.save
-    # @mentee_application_state.status = "Nexted"
-    redirect_to user_mentee_applications_path, notice: 'Successfully promoted.'
-   else
-    render some_error_path, alert: 'Failed to promote.'
-   end
+    if @user_mentee_application.promote_application(current_user)
+      redirect_to user_mentee_applications_path, notice: 'Successfully promoted to next stage.'
+    else
+      redirect_to user_mentee_applications_path, alert: 'Failed to promote.'
+    end
   end
 end
