0579120737b356681976075c053f2c9a53560297
Josh Smith <jmsmith1018@gmail.com>
2023-08-27 11:05:51 -0700

    Fix test expectations in profile link component spec

--- spec/components/profile/link_component_spec.rb
+++ spec/components/profile/link_component_spec.rb
@@ -19,33 +19,29 @@ RSpec.describe Profile::LinkComponent, type: :component do
     it 'renders the github link and icon when passed the github link type' do
       render_inline(described_class.new(link_type: :github_link, profile: profile_with_links))
 
-      within("a[href=#{profile_with_links.github_link}]") do
-        expect(page).to have_css('.fa-github')
-      end
+      expect(page).to have_link(href: profile_with_links.github_link)
+      expect(page).to have_css('.fa-github')
     end
 
     it 'renders the linkedin link and icon when passed the linkedin link type' do
       render_inline(described_class.new(link_type: :linked_in_link, profile: profile_with_links))
 
-      within("a[href=#{profile_with_links.linked_in_link}]") do
-        expect(page).to have_css('.fa-linkedin-in')
-      end
+      expect(page).to have_link(href: profile_with_links.linked_in_link)
+      expect(page).to have_css('.fa-linkedin-in')
     end
 
     it 'renders the personal site link and link icon when passed the personal site link type' do
       render_inline(described_class.new(link_type: :personal_site_link, profile: profile_with_links))
 
-      within("a[href=#{profile_with_links.personal_site_link}]") do
-        expect(page).to have_css('.fa-link')
-      end
+      expect(page).to have_link(href: profile_with_links.personal_site_link)
+      expect(page).to have_css('.fa-link')
     end
 
     it 'renders the twitter link and icon when passed the twitter link type' do
       render_inline(described_class.new(link_type: :twitter_link, profile: profile_with_links))
 
-      within("a[href=#{profile_with_links.twitter_link}]") do
-        expect(page).to have_css('.fa-twitter')
-      end
+      expect(page).to have_link(href: profile_with_links.twitter_link)
+      expect(page).to have_css('.fa-twitter')
     end
   end
 end
