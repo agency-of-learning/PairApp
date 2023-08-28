e2b3de70355280ae468e41013e3c95b0175e3f27
Josh Smith <jmsmith1018@gmail.com>
2023-08-28 12:21:10 -0700

    Customize after sign in path for properly directing applicants

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,11 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  protected
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def after_sign_in_path_for(user)
+    return super(user) unless user.applicant?
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    user.mentee_application || new_user_mentee_application_path
+  end
 end
