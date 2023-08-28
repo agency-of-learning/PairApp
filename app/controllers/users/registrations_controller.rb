9338fd5de57aa9c97476c1d3e21c5aef605c4549
Josh Smith <jmsmith1018@gmail.com>
2023-08-28 12:41:37 -0700

    Configure devise parameter sanitization including sign up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,18 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-
-  # POST /resource
-  def create
-    super(&:applicant!)
-  end
-
   protected
-
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[time_zone first_name last_name])
-  end
 
   def after_sign_up_path_for(_resource)
     new_user_mentee_application_path
