db53e9b2df8822636886c8f02cb8358315fe4c47
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-08-29 11:09:55 -0700

    re adds  promote_application method to create action

--- app/controllers/user_mentee_applications/promotions_controller.rb
+++ app/controllers/user_mentee_applications/promotions_controller.rb
@@ -2,7 +2,7 @@ class UserMenteeApplications::PromotionsController < ApplicationController
   def create
     @user_mentee_application = UserMenteeApplication.find(params[:user_mentee_application_id])
 
-    if @user_mentee_application.save
+    if @user_mentee_application.promote_application(current_user)
       redirect_to user_mentee_applications_path, notice: 'Successfully promoted to next stage.'
     else
       redirect_to user_mentee_applications_path, alert: 'Failed to promote.'
