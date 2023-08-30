556088f3c29b57abb3587bc95ba299a6d93be5cf
Ariel Fogel <fogeltine@gmail.com>
2023-08-30 14:17:41 +0300

    fix js linting

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,11 @@
-import "@fortawesome/fontawesome-free/js/all";
-import "@hotwired/turbo-rails";
-import "./controllers";
-import "trix";
-import "@rails/actiontext";
-import "./src/direct_uploads";
-import "./src/inline_code";
+import '@fortawesome/fontawesome-free/js/all';
+import '@hotwired/turbo-rails';
+import './controllers';
+import 'trix';
+import '@rails/actiontext';
+import './src/direct_uploads';
+import './src/inline_code';
 
-import * as ActiveStorage from "@rails/activestorage";
+import * as ActiveStorage from '@rails/activestorage';
+
 ActiveStorage.start();
