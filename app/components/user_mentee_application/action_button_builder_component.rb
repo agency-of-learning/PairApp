fea1b3601d7ee2040bc3a726388fd8e077442971
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-08-30 19:17:49 -0700

    removed render_button and added its logic to render_promote_button

--- app/components/user_mentee_application/action_button_builder_component.rb
+++ app/components/user_mentee_application/action_button_builder_component.rb
@@ -7,12 +7,9 @@ class UserMenteeApplication::ActionButtonBuilderComponent < ViewComponent::Base
     @mentee_application = mentee_application
   end
 
-  def render_button
-    return unless @mentee_application.can_promote?
-    render_promote_button
-  end
-
   def render_promote_button
+    return unless @mentee_application.can_promote?
+
     button_to 'Promote to next stage', user_mentee_application_promotions_path(@mentee_application),
       method: :post,
       class: 'btn btn-primary capitalize btn-link btn-xs sm:btn-sm hover:no-underline'
