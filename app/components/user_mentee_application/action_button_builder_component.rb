d521e385d8c01742e901c919e2b2e2fcc4856e72
Ariel Fogel <fogeltine@gmail.com>
2023-09-05 16:01:11 +0300

    remove dead code

--- app/components/user_mentee_application/action_button_builder_component.rb
+++ app/components/user_mentee_application/action_button_builder_component.rb
@@ -9,17 +9,4 @@ class UserMenteeApplication::ActionButtonBuilderComponent < ViewComponent::Base
   private
 
   attr_reader :mentee_application, :current_user
-
-  def render_promote_button
-    return unless @mentee_application.current_state.can_promote?
-  end
-
-  def render_reject_button
-    last_state = @mentee_application.mentee_application_states.last
-    return if last_state.rejected?
-
-    button_to 'Reject', user_mentee_application_rejections_path(@mentee_application),
-      method: :post,
-      class: 'btn btn-primary capitalize btn-link btn-xs sm:btn-sm hover:no-underline'
-  end
 end
