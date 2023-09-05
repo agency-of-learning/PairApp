f1d97b3ebed08e25e537efdec092bdccc1c61f75
toyhammered <daniel@rassiner.com>
2023-09-04 20:25:26 -0700

    small sentry fix

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = ENV.fetch('SENTRY_DSN')
+  config.dsn = ENV.fetch('SENTRY_DSN', nil)
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
   config.enabled_environments = %w[production]
