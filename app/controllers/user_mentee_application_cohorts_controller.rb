266f78d4eed8ed35c79f7a94ba2ef26c39891dc9
Josh Smith <jmsmith1018@gmail.com>
2023-09-05 21:03:10 -0700

    Add show page for mentee application cohorts

--- app/controllers/user_mentee_application_cohorts_controller.rb
+++ app/controllers/user_mentee_application_cohorts_controller.rb
@@ -3,4 +3,10 @@ class UserMenteeApplicationCohortsController < ApplicationController
     @active_cohort = UserMenteeApplicationCohort.active
     @inactive_cohorts = UserMenteeApplicationCohort.inactive
   end
+
+  def show
+    @cohort = UserMenteeApplicationCohort
+              .includes(:user_mentee_applications)
+              .find(params[:id])
+  end
 end
