28dc474683d8cb113447a582761b375905a35e7c
Josh Smith <jmsmith1018@gmail.com>
2023-09-06 17:41:43 -0700

    Add authorization for cohorts controller

--- app/controllers/user_mentee_application_cohorts_controller.rb
+++ app/controllers/user_mentee_application_cohorts_controller.rb
@@ -1,4 +1,6 @@
 class UserMenteeApplicationCohortsController < ApplicationController
+  before_action -> { authorize :user_only, :admin? }
+
   def index
     @active_cohort = UserMenteeApplicationCohort.active
     @inactive_cohorts = UserMenteeApplicationCohort.inactive
