27d37b2ef48abac1c9253d39668e89cada027d9e
Josh Smith <jmsmith1018@gmail.com>
2023-09-08 13:56:28 -0700

    Add modal state form to application index
    Co-authored-by: Gustavo Valenzuela <gustavo.devvv@gmail.com>

--- app/components/user_mentee_application/action_button_builder_component.rb
+++ app/components/user_mentee_application/action_button_builder_component.rb
@@ -7,25 +7,12 @@ class UserMenteeApplication::ActionButtonBuilderComponent < ViewComponent::Base
     @mentee_application = mentee_application
   end
 
-  def render_promote_button
-    return unless @mentee_application.current_state.can_promote?
-
-    button_to 'Promote to next stage', user_mentee_application_promotions_path(@mentee_application),
-      method: :post,
-      class: 'btn btn-primary capitalize btn-link btn-xs sm:btn-sm hover:no-underline'
-  end
-
-  def render_reject_button
-    last_state = @mentee_application.mentee_application_states.last
-    return if last_state&.rejected?
-
-    button_to 'Reject', user_mentee_application_rejections_path(@mentee_application),
-      method: :post,
-      class: 'btn btn-primary capitalize btn-link btn-xs sm:btn-sm hover:no-underline'
-  end
-
   def call
-    concat(render_promote_button)
-    concat(render_reject_button)
+    link_to(
+      'Action',
+      new_user_mentee_application_mentee_application_state_path(@mentee_application),
+      class: 'link link-primary',
+      data: { turbo_frame: 'modal' }
+    )
   end
 end
