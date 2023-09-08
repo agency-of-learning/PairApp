f0bc2209771899e86db35cb976803e2aade2aab6
Ariel <fogeltine@gmail.com>
2023-09-08 23:14:16 +0300

    scope pair requests to active members (#291)
    * scope pair requests to active members
    
    * also include admin
    
    * sacrifice to linter gods

--- app/models/user.rb
+++ app/models/user.rb
@@ -83,6 +83,8 @@ class User < ApplicationRecord
 
   after_create :create_profile!
 
+  scope :members, -> { where(role: %i[member admin]) }
+
   def self.invite!(attributes = {}, invited_by = nil, options = {}, &)
     default_name = { first_name: 'First', last_name: 'Last' }
     super(attributes.merge(default_name), invited_by, options, &)
