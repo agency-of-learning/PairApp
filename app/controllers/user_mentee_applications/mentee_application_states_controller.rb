290680089013c6ee1b777d4a0b35b37b69f4e256
Josh Smith <jmsmith1018@gmail.com>
2023-09-07 19:01:12 -0700

    Rework application state controller to work with new features

--- app/controllers/user_mentee_applications/mentee_application_states_controller.rb
+++ app/controllers/user_mentee_applications/mentee_application_states_controller.rb
@@ -1,17 +1,33 @@
 class UserMenteeApplications::MenteeApplicationStatesController < ApplicationController
+  before_action :load_user_mentee_application
+
+  def new
+    @mentee_application_state = @user_mentee_application.mentee_application_states.build
+  end
+
   def create
-    @user_mentee_application = UserMenteeApplication.find(params[:user_mentee_application_id])
+    @mentee_application_state = @user_mentee_application.mentee_application_states.build(mentee_application_state_params)
 
-    @user_mentee_application.mentee_application_states.create!(
-      mentee_application_state_params.merge({ status_changed_id: current_user.id })
-    )
-
-    redirect_to user_mentee_applications_path, notice: "Application updated to #{@user_mentee_application.current_status}"
+    respond_to do |format|
+      if @mentee_application_state.save
+        format.html { redirect_to user_mentee_applications_path }
+        format.turbo_stream { flash.now[:notice] = "Application updated to #{@mentee_application_state.status}" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
 
+  def load_user_mentee_application
+    @user_mentee_application = UserMenteeApplication.find(params[:user_mentee_application_id])
+  end
+
   def mentee_application_state_params
-    params.require(:mentee_application_state).permit(:status, :note)
+    params
+      .require(:mentee_application_state)
+      .permit(:status, :note)
+      .merge(status_changed_id: current_user.id)
   end
 end
