a2f0e911f44e8aaad167c9f6a709b31253c09380
toyhammered <daniel@rassiner.com>
2023-09-10 14:48:36 -0700

    update navbar based on user role.

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,15 @@
 class LandingController < ApplicationController
   skip_before_action :authenticate_user!
-  def index; end
+
+  def index
+    if current_user.blank?
+      render :index
+    elsif current_user.applicant?
+      # TODO: we technically allow multiple applications but that should be fixed really soon.
+      redirect_to user_mentee_application_path(current_user.mentee_application)
+    elsif current_user.member? || current_user.admin?
+      # TODO: we should have a dashboard home page (the one that shows your standups, pair requests, etc..) for the day
+      redirect_to dashboards_path
+    end
+  end
 end
