297ca6d848d7de9b8de1ba78f60f779559351630
Ariel Fogel <fogeltine@gmail.com>
2023-09-12 14:34:02 +0300

    make sure that base templates render

--- spec/mailers/mentee_application_mailer_spec.rb
+++ spec/mailers/mentee_application_mailer_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe MenteeApplicationMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:recipient) { create(:user) }
+
+  describe '#notify_for_acceptance' do
+    subject(:mail) { described_class.with(recipient:).notify_for_acceptance }
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq('Welcome to the Agency of Learning!')
+      expect(mail.to).to eq([recipient.email])
+      expect(mail.from).to eq(['no_reply@agencyoflearning.com'])
+    end
+  end
+
+  describe '#notify_for_rejection' do
+    subject(:mail) { described_class.with(recipient:).notify_for_rejection }
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq('Update on your application to the Agency of Learning')
+      expect(mail.to).to eq([recipient.email])
+      expect(mail.from).to eq(['no_reply@agencyoflearning.com'])
+    end
+  end
 end
