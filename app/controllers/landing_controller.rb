82c21a7215776ad203e274a28cca9f5c79e3310b
Josh Smith <jmsmith1018@gmail.com>
2023-09-12 10:45:25 -0700

    Fix landing page for applicants

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -5,8 +5,7 @@ class LandingController < ApplicationController
     if current_user.blank?
       render :index
     elsif current_user.applicant?
-      # TODO: we technically allow multiple applications but that should be fixed really soon.
-      redirect_to user_mentee_application_path(current_user.mentee_application)
+      redirect_to user_mentee_applications_path
     elsif current_user.member? || current_user.admin?
       # TODO: we should have a dashboard home page (the one that shows your standups, pair requests, etc..) for the day
       redirect_to dashboards_path
