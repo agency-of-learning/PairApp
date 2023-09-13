5cc6a5a10027f87f99c3deff8b8fe9aa54fdeae7
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-09-13 12:25:20 -0700

    UserMenteeApplicationMailer with application link in template passing test

--- app/mailers/user_mentee_application_mailer.rb
+++ app/mailers/user_mentee_application_mailer.rb
@@ -1,7 +1,6 @@
 class UserMenteeApplicationMailer < ApplicationMailer
   def notify_for_application_submission
     @user_mentee_application = params[:user_mentee_application]
-    @user = @user_mentee_application.user
-    mail(to: @user.email, subject: 'Mentee Application Submitted')
+    mail(subject: 'Mentee Application Submitted')
   end
 end
--- spec/mailers/user_mentee_application_spec.rb
+++ spec/mailers/user_mentee_application_spec.rb
@@ -1,17 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe UserMenteeApplicationMailer do
-  let(:mentee_application) { create(:user_mentee_application) }
-  let(:recipient) { mentee_application.user }
+  let(:application) { create(:user_mentee_application) }
+  let(:recipient) { application.user }
 
   describe '#notify_for_application_submission' do
-    subject(:mail) { described_class.notify_for_application_submission(mentee_application) }
+    subject(:mail) { described_class.with(application:, recipient:).notify_for_application_submission }
 
     it 'renders the headers' do
-      expect(:mail.subject).to eq('Mentee Application Submitted')
-      expect(:mail.to).to eq([recipient.email])
+      expect(mail.subject).to eq('Mentee Application Submitted')
+      expect(mail.to).to eq([recipient.email])
       expect(mail.from).to eq(['no_reply@agencyoflearning.com'])
     end
   end
-
 end
