b0ca634125fb40112db20e348da08ebc6a906818
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-09-13 13:05:12 -0700

    Adds specs for UserMenteeApplicationAlertMailer and and fixes UserMenteeApplicationMailer spec)

--- app/mailers/user_mentee_application_alert_mailer.rb
+++ app/mailers/user_mentee_application_alert_mailer.rb
@@ -1,7 +1,6 @@
 class UserMenteeApplicationAlertMailer < ApplicationMailer
   def notify_for_application_submission
-    @user_mentee_application = params[:user_mentee_application]
-    @user = User.admin.first
-    mail(to: @user.email, subject: 'Mentee Application Submitted')
+    @user_mentee_application = params[:application]
+    mail(subject: 'New Mentee Application Submitted')
   end
 end
--- spec/mailers/user_mentee_application_alert_spec.rb
+++ spec/mailers/user_mentee_application_alert_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe UserMenteeApplicationAlertMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:application) { create(:user_mentee_application) }
+  let(:recipient) { application.user }
+
+  describe '#notify_for_application_submission' do
+    subject(:mail) { described_class.with(recipient: recipient, application: application).notify_for_application_submission }
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq('New Mentee Application Submitted')
+      expect(mail.to).to eq([recipient.email])
+      expect(mail.from).to eq(['no_reply@agencyoflearning.com'])
+    end
+  end
 end
