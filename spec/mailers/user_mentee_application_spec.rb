4e6c03c48affd6505c338bd022b1cf37040c28c1
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-09-13 10:08:34 -0700

    Adds User mentee application mailer spec)

--- spec/mailers/user_mentee_application_spec.rb
+++ spec/mailers/user_mentee_application_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe UserMenteeApplicationMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:mentee_application) { create(:user_mentee_application) }
+  let(:recipient) { mentee_application.user }
+
+  describe '#notify_for_application_submission' do
+    subject(:mail) { described_class.notify_for_application_submission(mentee_application) }
+
+    it 'renders the headers' do
+      expect(:mail.subject).to eq('Mentee Application Submitted')
+      expect(:mail.to).to eq([recipient.email])
+      expect(mail.from).to eq(['no_reply@agencyoflearning.com'])
+    end
+  end
+
 end
