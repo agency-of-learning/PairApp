c35eeb5f02733a8f21bf0c00e77095d4ff2dfb3d
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-09-14 13:56:41 -0700

    Adds enqueue option to notification objects

--- app/notifications/user_mentee_application/application_submission_alert.rb
+++ app/notifications/user_mentee_application/application_submission_alert.rb
@@ -1,7 +1,8 @@
 class UserMenteeApplication::ApplicationSubmissionAlert < Noticed::Base
   deliver_by :email,
     mailer: 'UserMenteeApplicationAlertMailer',
-    method: :notify_for_application_submission
-
+    method: :notify_for_application_submission,
+    enqueue: true
+   
   param :user_mentee_application
 end
