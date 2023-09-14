0fbd623e519e1e7fc49df60f6d2f9e9faf39fe7b
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-09-14 08:20:37 -0700

    deleted preview files

--- spec/mailers/previews/user_mentee_application_alert_preview.rb
+++
@@ -1,3 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mentee_application_alert
-class UserMenteeApplicationAlertPreview < ActionMailer::Preview
-end
--- spec/mailers/previews/user_mentee_application_preview.rb
+++
@@ -1,3 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mentee_application
-class UserMenteeApplicationPreview < ActionMailer::Preview
-end
