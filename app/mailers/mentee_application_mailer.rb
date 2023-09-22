b3f22d58c1c3ff34ec78799b5f321001087d8bc4
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-09-22 16:22:58 -0700

    removes duplicated lines in UserMenteeApplicationMailer and MenteeApplicationMailer

--- app/mailers/mentee_application_mailer.rb
+++ app/mailers/mentee_application_mailer.rb
@@ -1,8 +1,6 @@
 class MenteeApplicationMailer < ApplicationMailer
   default from: 'dave@agencyoflearning.com', bcc: 'dave@agencyoflearning.com'
 
-  default from: 'dave@agencyoflearning.com'
-
   def notify_for_acceptance
     @application = params[:application]
     mail(subject: 'Welcome to the Agency of Learning!')
