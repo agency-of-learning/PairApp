d4cc7643ee81ec7da026443962ec674729e02121
Ariel Fogel <fogeltine@gmail.com>
2023-09-22 16:53:58 +0300

    add mailer and notification

--- app/mailers/mentee_application_mailer.rb
+++ app/mailers/mentee_application_mailer.rb
@@ -17,4 +17,9 @@ class MenteeApplicationMailer < ApplicationMailer
   def notify_for_code_challenge_approved
     mail(subject: 'Moving forward in the application process for the Agency of Learning')
   end
+
+  def notify_for_reapplication
+    @active_cohort_name = params[:active_cohort_name]
+    mail(subject: 'Invitation to Apply to the Agency of Learning')
+  end
 end
