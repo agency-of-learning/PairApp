305f4bdf1d8d6427f204c206f886213e053e1cbe
Josh Smith <jmsmith1018@gmail.com>
2023-09-25 09:41:51 -0700

    Add container component for new navigation system

---
+++ app/components/navigation/container_component.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Navigation::ContainerComponent < ViewComponent::Base
+  renders_many :primary_links, ->(text:, path:) { tag.li link_to(text, path) }
+  renders_many :dropdown_links, ->(text:, path:, **opts) { tag.li link_to(text, path), **opts }
+
+  renders_one :profile_picture
+
+  renders_one :page_content
+
+  def initialize(home_path:)
+    @home_path = home_path
+  end
+
+  private
+
+  attr_reader :home_path
+  alias_method :render_dropdown?, :profile_picture?
+end
