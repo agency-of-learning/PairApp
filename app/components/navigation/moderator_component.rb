6d05f45c6c5c903cfb16db962210ca443a7a7cdc
Josh Smith <jmsmith1018@gmail.com>
2023-09-25 12:32:31 -0700

    Add navigation component for moderators

---
+++ app/components/navigation/moderator_component.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class Navigation::ModeratorComponent < NavigationComponent
+  private
+
+  def home_path
+    dashboards_path
+  end
+
+  def primary_links
+    [
+      { text: 'Standups', path: standup_meeting_groups_path },
+      { text: 'Pair Requests', path: pair_requests_path },
+      { text: 'Agency Blog', path: blog_posts_path },
+      { text: 'Applications Portal', path: user_mentee_application_cohorts_path }
+    ]
+  end
+
+  def dropdown_links
+    [
+      { text: 'My Profile', path: profile_path(user) },
+      { text: 'Feedback', path: feedbacks_path },
+      { text: 'My Blog', path: blog_path(user.blog_slug), class: 'border-b border-neutral' },
+      { text: 'Account Settings', path: edit_user_registration_path },
+      { text: 'Logout', path: destroy_user_session_path }
+    ]
+  end
+end
