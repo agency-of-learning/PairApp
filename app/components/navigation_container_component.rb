76b4c12a6ac828e0fc0d0e3730222a7d07766623
Josh Smith <jmsmith1018@gmail.com>
2023-09-26 10:40:34 -0700

    Pad first and last items in dropdown nav

--- app/components/navigation_container_component.rb
+++ app/components/navigation_container_component.rb
@@ -2,7 +2,15 @@
 
 class NavigationContainerComponent < ViewComponent::Base
   renders_many :primary_links, ->(text:, path:) { tag.li link_to(text, path) }
-  renders_many :dropdown_links, ->(text:, path:, **opts) { tag.li link_to(text, path), **opts }
+
+  renders_many :dropdown_links, ->(text:, path:, **opts) do
+    class_names = opts.delete(:class)
+    tag.li(
+      link_to(text, path),
+      class: "first:pt-3 last:pb-3 rounded-none #{class_names}",
+      **opts
+    )
+  end
 
   renders_one :profile_picture
 
