137b426b0e5829891ec035c9874eb89e6c8cf9a6
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-09-28 11:32:33 -0700

    linting

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -3,5 +3,4 @@ Time::DATE_FORMATS[:hour_min_period] = '%-l:%M%P'
 Time::DATE_FORMATS[:slash_format] = '%-m/%-d/%Y'
 Time::DATE_FORMATS[:slash_format_with_time] = '%-m/%-d/%Y %I:%M %Z'
 
-
 Date::DATE_FORMATS[:slash_format] = '%-m/%-d/%Y'
