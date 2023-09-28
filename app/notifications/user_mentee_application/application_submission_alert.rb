77892f90e4fab62c2bab536237c76a07d158023b
Ariel Fogel <fogeltine@gmail.com>
2023-09-28 13:59:33 +0300

    remove unnecessary notifications

--- app/notifications/user_mentee_application/application_submission_alert.rb
+++
@@ -1,8 +0,0 @@
-class UserMenteeApplication::ApplicationSubmissionAlert < Noticed::Base
-  deliver_by :email,
-    mailer: 'UserMenteeApplicationMailer',
-    method: 'notify_for_application_submission',
-    enqueue: true
-
-  param :application
-end
