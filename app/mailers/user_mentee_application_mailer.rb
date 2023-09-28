dde40c55a464e133d3c78ab9379642086a6c0616
Ariel Fogel <fogeltine@gmail.com>
2023-09-28 13:58:02 +0300

    remove unnecessary mailers

--- app/mailers/user_mentee_application_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMenteeApplicationMailer < ApplicationMailer
-  default from: 'dave@agencyoflearning.com', bcc: 'dave@agencyoflearning.com'
-
-  def notify_for_application_submission
-    @user_mentee_application = params[:user_mentee_application]
-    mail(subject: 'Woohoo! Your Application’s In - Agency of Learning.')
-  end
-end
