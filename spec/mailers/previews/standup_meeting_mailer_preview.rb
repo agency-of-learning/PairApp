870efe109b0f81dff691871ea136802d05a5f960
Ariel Fogel <fogeltine@gmail.com>
2023-09-28 14:48:27 +0300

    update preview to use last standupmeeting

--- spec/mailers/previews/standup_meeting_mailer_preview.rb
+++ spec/mailers/previews/standup_meeting_mailer_preview.rb
@@ -1,11 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/standup_meeting_mailer
 class StandupMeetingMailerPreview < ActionMailer::Preview
   def notify_member_of_daily_meeting
-    standup_meeting_group = StandupMeetingGroup.last
-    user = standup_meeting_group.users.last
-    standup_meeting = StandupMeeting.includes(:user).find_or_initialize_by(
-      standup_meeting_group:, user:, meeting_date: Date.current
-    )
-    StandupMeetingMailer.with(recipient: user, standup_meeting:).notify_member_of_daily_meeting
+    standup_meeting = StandupMeeting.last
+    recipient = standup_meeting.user
+    StandupMeetingMailer.with(recipient:, standup_meeting:).notify_member_of_daily_meeting
   end
 end
