818860e812df6154acd3eb01840bbb20311ad949
toyhammered <daniel@rassiner.com>
2023-10-17 17:28:06 -0700

    fix migration to power through invalid applications

--- db/migrate/20231015020025_update_mentee_application_updated_at.rb
+++ db/migrate/20231015020025_update_mentee_application_updated_at.rb
@@ -3,6 +3,9 @@ class UpdateMenteeApplicationUpdatedAt < ActiveRecord::Migration[7.0]
     UserMenteeApplication.all.each do |application|
       new_updated_at = application.current_state.updated_at
       application.update!(updated_at: new_updated_at)
+    rescue StandardError
+      # We have some applications that have invalid data (most likely from migration and some pre validation stuff)
+      nil
     end
   end
 
