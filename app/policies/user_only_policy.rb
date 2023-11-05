8ad94c425705893f7984a0a5dcb240085fe6007b
JP <85654561+jp524@users.noreply.github.com>
2023-11-05 14:32:06 -0500

    Add #agent? to UserOnlyPolicy

--- app/policies/user_only_policy.rb
+++ app/policies/user_only_policy.rb
@@ -1,6 +1,10 @@
 class UserOnlyPolicy < ApplicationPolicy
   attr_reader :user
 
+  def agent?
+    User::AGENT_ROLES.include?(user.role)
+  end
+
   # the delegate method forwards the admin? method call @user's admin? method
   delegate :admin?, :applicant?, to: :user
 end
