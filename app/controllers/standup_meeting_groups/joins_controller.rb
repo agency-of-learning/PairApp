bf8ffab045a0c495ff712a5c0428c2e76ecd7ab6
Won Rhim <wwrk22@gmail.com>
2023-11-13 12:32:32 -0500

    fix: handle the uniqueness validation failure

--- app/controllers/standup_meeting_groups/joins_controller.rb
+++ app/controllers/standup_meeting_groups/joins_controller.rb
@@ -1,4 +1,5 @@
 class StandupMeetingGroups::JoinsController < ApplicationController
+  # POST /standup_meeting_groups/:standup_meeting_group_id/joins.turbo_stream
   def create
     @standup_meeting_group = StandupMeetingGroup.find(params[:standup_meeting_group_id])
     @standup_meeting_group_user = @standup_meeting_group.standup_meeting_groups_users.build(user: current_user)
@@ -7,12 +8,8 @@ class StandupMeetingGroups::JoinsController < ApplicationController
 
     authorize @standup_meeting_group_user, policy_class: StandupMeetingGroup::JoinPolicy
 
-    @standup_meeting_group_user.save!
-
-    respond_to do |format|
-      format.turbo_stream do
-        flash.now[:notice] = "You have joined #{@standup_meeting_group.name}!"
-      end
+    if @standup_meeting_group_user.save
+      flash.now[:notice] = "You have joined #{@standup_meeting_group.name}!"
     end
   end
 
