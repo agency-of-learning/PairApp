cfb5254e198c70aa2ba50cc323cbded366d30203
Won Rhim <wwrk22@gmail.com>
2023-11-14 10:05:21 -0500

    feat: add migration to remove duplicate records before replacing indexes

---
+++ db/migrate/20231114144505_make_indexes_unique_for_standup_meeting_group_users.rb
@@ -0,0 +1,28 @@
+class MakeIndexesUniqueForStandupMeetingGroupUsers < ActiveRecord::Migration[7.0]
+  def change
+    # Remove duplicate records.
+    unique_ids = StandupMeetingGroupUser.group(
+      :standup_meeting_group_id, :user_id
+    ).pluck('MIN(id)')
+
+    StandupMeetingGroupUser.where.not(id: unique_ids).delete_all
+
+    change_table :standup_meeting_groups_users do |t|
+      # Keep the old indexes until new ones are added for safety.
+      t.rename_index :index_smg_users_on_smg_id_and_user_id,
+        :index_smg_users_on_smg_id_and_user_id_temp
+      t.rename_index :index_smg_users_on_user_id_and_smg_id,
+        :index_smg_users_on_user_id_and_smg_id_temp
+
+      t.index [:standup_meeting_group_id, :user_id],
+        unique: true,
+        name: :index_smg_users_on_smg_id_and_user_id
+      t.index [:user_id, :standup_meeting_group_id],
+        unique: true,
+        name: :index_smg_users_on_user_id_and_smg_id
+
+      t.remove_index name: :index_smg_users_on_smg_id_and_user_id_temp
+      t.remove_index name: :index_smg_users_on_user_id_and_smg_id_temp
+    end
+  end
+end
