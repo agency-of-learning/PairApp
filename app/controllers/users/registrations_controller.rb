0f6201edf4f290aa76acd2d060eee044903dc482
Josh Smith <jmsmith1018@gmail.com>
2023-11-15 20:45:26 -0800

    Add information to sign up page when no cohort is active

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   skip_before_action :only_authorize_agent
 
+  def new
+    @active_cohort_flag = UserMenteeApplicationCohort.active.present?
+    super
+  end
+
   protected
 
   def after_sign_up_path_for(_resource)
