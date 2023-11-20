6799d82210be83a1c102151ca0f59fef41dfcb4f
Natblow <natblow@hotmail.fr>
2023-11-20 16:03:31 -0500

    add slug attribute to MenteeApplicationState model

--- app/models/mentee_application_state.rb
+++ app/models/mentee_application_state.rb
@@ -4,6 +4,7 @@
 #
 #  id                         :bigint           not null, primary key
 #  note                       :text
+#  slug                       :string
 #  status                     :integer          default("application_received"), not null
 #  created_at                 :datetime         not null
 #  updated_at                 :datetime         not null
@@ -14,6 +15,7 @@
 #
 #  index_mentee_application_states_on_status_changed_id           (status_changed_id)
 #  index_mentee_application_states_on_user_mentee_application_id  (user_mentee_application_id)
+#  index_on_slug_and_user_mentee_application_id                   (slug,user_mentee_application_id) UNIQUE
 #
 # Foreign Keys
 #
@@ -21,10 +23,15 @@
 #  fk_rails_...  (user_mentee_application_id => user_mentee_applications.id)
 #
 class MenteeApplicationState < ApplicationRecord
+  extend FriendlyId
   belongs_to :user_mentee_application, touch: true
   # rubocop:disable Rails/InverseOf
   belongs_to :reviewer, class_name: 'User', foreign_key: :status_changed_id, optional: true
   # rubocop:enable Rails/InverseOf
+
+  friendly_id :status, use: :scoped, scope: :user_mentee_application
+
+  validates :slug, uniqueness: { scope: :user_mentee_application_id }
 
   enum status: {
     application_received: 0,
