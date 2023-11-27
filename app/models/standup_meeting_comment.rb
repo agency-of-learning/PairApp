8fffecc9afd9bec30aaa75993fc955c2fb30a0cc
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-11-27 11:20:39 -0800

    Add enum for standup_meeting_comment sections

--- app/models/standup_meeting_comment.rb
+++ app/models/standup_meeting_comment.rb
@@ -1,3 +1,9 @@
 class StandupMeetingComment < ApplicationRecord
   belongs_to :standup_meeting
+
+  enum section: {
+    yesterday_work_description: 0,
+    today_work_description: 1,
+    blockers_description: 2
+  }
 end
