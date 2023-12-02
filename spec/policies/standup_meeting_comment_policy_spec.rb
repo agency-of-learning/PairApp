f9b21ee09bcb2a76faaf3764abda54fdff5f10c4
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-12-02 15:18:33 -0800

    Add policy test specs for standup_meeting_comment destroy?

--- spec/policies/standup_meeting_comment_policy_spec.rb
+++ spec/policies/standup_meeting_comment_policy_spec.rb
@@ -3,25 +3,23 @@ require 'rails_helper'
 RSpec.describe StandupMeetingCommentPolicy, type: :policy do
   subject { described_class }
 
-  let(:user) { User.new }
+  let(:member) { build(:user) }
+  # let(:not_author_member) { build(:user) }
 
-  permissions '.scope' do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+  let(:not_owned_comment) { build(:standup_meeting_comment) }
+  let(:authored_comment) { build(:standup_meeting_comment, user: member) }
 
   permissions :update? do
     pending "add some examples to (or delete) #{__FILE__}"
   end
 
   permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it 'denies access if user is not author of comment' do
+      expect(subject).not_to permit(member, not_owned_comment)
+    end
+
+    it 'grants access if user is author of comment' do
+      expect(subject).to permit(member, authored_comment)
+    end
   end
 end
