869b1e66e701b8945e74ab0a873f244e01414bb4
Won Rhim <wwrk22@gmail.com>
2023-12-08 12:00:26 -0500

    feat: allow RichTextReaction to interface with a limited set of emojis and captions

--- app/models/rich_text_reaction.rb
+++ app/models/rich_text_reaction.rb
@@ -19,7 +19,25 @@
 #  fk_rails_...  (rich_text_id => action_text_rich_texts.id)
 #  fk_rails_...  (user_id => users.id)
 #
+require "emoji"
+
 class RichTextReaction < ApplicationRecord
+  include Emoji
+
   belongs_to :user
   belongs_to :rich_text, class_name: "ActionText::RichText"
+
+  # Return the text description of an emoji. Return nil if the emoji is not in
+  # the set of emojis defined in the Emoji module.
+  # e.g. "thumbs-up" for "👍"
+  def emoji_caption
+    caption(emoji)
+  end
+
+  class << self
+    # Return the Emoji module's limited hash of emoji-to-caption pairs.
+    def emojis
+      Emoji::EMOJIS
+    end
+  end
 end
