12c40b3bb8c72c4664095976d6fcc5e8c19533b1
Won Rhim <wwrk22@gmail.com>
2023-12-08 09:52:36 -0500

    feat: rollback and remigrate RichTextReaction creation with emoji and user_id columns

--- app/models/rich_text_reaction.rb
+++ app/models/rich_text_reaction.rb
@@ -3,18 +3,23 @@
 # Table name: rich_text_reactions
 #
 #  id           :bigint           not null, primary key
+#  emoji        :string           not null
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #  rich_text_id :bigint           not null
+#  user_id      :bigint           not null
 #
 # Indexes
 #
 #  index_rich_text_reactions_on_rich_text_id  (rich_text_id)
+#  index_rich_text_reactions_on_user_id       (user_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (rich_text_id => action_text_rich_texts.id)
+#  fk_rails_...  (user_id => users.id)
 #
 class RichTextReaction < ApplicationRecord
+  belongs_to :user
   belongs_to :rich_text, class_name: "ActionText::RichText"
 end
--- db/migrate/20231207143236_create_rich_text_reactions.rb
+++ db/migrate/20231207143236_create_rich_text_reactions.rb
@@ -1,7 +1,9 @@
 class CreateRichTextReactions < ActiveRecord::Migration[7.0]
   def change
     create_table :rich_text_reactions do |t|
+      t.references :user, null: false, foreign_key: true
       t.references :rich_text, null: false, foreign_key: { to_table: :action_text_rich_texts }
+      t.string :emoji, null: false
 
       t.timestamps
     end
