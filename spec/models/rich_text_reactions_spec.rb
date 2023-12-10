6c43af222531de5c8e7f8739ec4f2665bdd951e9
Won Rhim <wwrk22@gmail.com>
2023-12-10 11:30:25 -0500

    refactor: Emoji class isn't needed

--- spec/models/rich_text_reactions_spec.rb
+++ spec/models/rich_text_reactions_spec.rb
@@ -1,20 +1,27 @@
 require "rails_helper"
 
 RSpec.describe RichTextReaction do
+  it "is valid with an emoji in the permissible set" do
+    thumbs_up = described_class.emojis.keys.first
+    rich_text_reaction = described_class.new(emoji: thumbs_up)
+
+    expect(rich_text_reaction.errors[:emoji]).to be_empty
+  end
+
+  it "is invalid with an emoji not in the permissible set" do
+    rich_text_reaction = described_class.new(emoji: "🥸")
+    rich_text_reaction.valid?
+
+    expect(rich_text_reaction.errors[:emoji]).to be_present
+  end
+
   describe "#emoji_caption" do
     it "returns the text description of an emoji" do
       thumbs_up = described_class.emojis.keys.first
       thumbs_up_caption = described_class.emojis.values.first
-      rtr = described_class.new(emoji: thumbs_up)
+      rich_text_reaction = described_class.new(emoji: thumbs_up)
 
-      expect(rtr.emoji_caption).to eq(thumbs_up_caption)
-    end
-
-    it "returns nil if the emoji is not in the permissible set" do
-      disguised_face = "🥸"
-      rtr = described_class.new(emoji: disguised_face)
-
-      expect(rtr.emoji_caption).to be_nil
+      expect(rich_text_reaction.emoji_caption).to eq(thumbs_up_caption)
     end
   end
 end
