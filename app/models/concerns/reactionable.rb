135b63221f1da3803f32c58f6979a0d6421471be
Won Rhim <wwrk22@gmail.com>
2023-12-19 17:44:46 -0500

    feat: gather reaction info for all rich text of a class

---
+++ app/models/concerns/reactionable.rb
@@ -0,0 +1,14 @@
+module Reactionable
+  # Return all reactions, along with the IDs of users who made the reaction,
+  # for the given +ActionText::RichText+, +rich_text+.
+  def reactions(rich_text)
+    result = RichTextReaction.where(rich_text:).pluck(:emoji, :user_id)
+    # e.g. [["thumbs_up", 3], ["thinking", 1]]
+
+    result = result.group_by(&:shift)
+    # e.g. { "thumbs_up" => [[1], [4], [2]], "thinking" => [[3]] }
+
+    result.transform_values(&:flatten)
+    # e.g. { "thumbs_up" => [1, 4, 2], "thinking" => [3] }
+  end
+end
