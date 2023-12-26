62dbb9b35dcbe719c1c3f8c4bbe1ccf1538e7e0b
Won Rhim <wwrk22@gmail.com>
2023-12-26 14:30:07 -0500

    refactor: `Reactionable` is not a fitting way of DRYing up as of now

--- spec/models/rich_text_reactions_spec.rb
+++ spec/models/rich_text_reactions_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe RichTextReaction do
   it 'is valid with an emoji in the permissible set' do
-    emoji_caption = described_class.emoji_captions.first
+    emoji_caption = Emoji.emoji_captions.sample
     rich_text_reaction = described_class.new(emoji_caption:)
 
     expect(rich_text_reaction.errors[:emoji_caption]).to be_empty
@@ -15,13 +15,34 @@ RSpec.describe RichTextReaction do
     expect(rich_text_reaction.errors[:emoji_caption]).to be_present
   end
 
-  describe '#emoji' do
-    it 'returns the emoji for a caption' do
-      emoji_caption = described_class.emoji_captions.first
-      rich_text_reaction = described_class.new(emoji_caption:)
+  describe '.reactions' do
+    let!(:standup_meeting) { create(:standup_meeting) }
+    let!(:rich_text) { standup_meeting.yesterday_work_description }
 
-      expect(rich_text_reaction.emoji)
-        .to eq(Emojis::DICTIONARY[emoji_caption])
+    it 'returns all reaction info for an update' do
+      # Emulate a user reacting to her own update, and another user reacting to
+      # the first user's update.
+      user = standup_meeting.user
+      other_user = create(:user)
+      my_reaction = Emoji.emoji_captions.first
+      other_reaction = Emoji.emoji_captions.second
+      RichTextReaction.create(emoji_caption: my_reaction, user:, rich_text:)
+      RichTextReaction.create(
+        emoji_caption: other_reaction,
+        user: other_user,
+        rich_text:
+      )
+
+      reactions = described_class.reactions(rich_text)
+
+      # Output should tell who made which reaction.
+      expected_reactions = { my_reaction => [user.id], other_reaction => [other_user.id] }
+      expect(reactions).to eq(expected_reactions)
+    end
+
+    it 'returns an empty hash if an update has no reactions' do
+      reactions = described_class.reactions(rich_text)
+      expect(reactions).to be_empty
     end
   end
 end
