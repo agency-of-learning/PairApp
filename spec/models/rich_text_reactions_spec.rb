9e38c27e2d260fe8e0cc42d0b3121bdfead18316
Won Rhim <wwrk22@gmail.com>
2023-12-31 15:58:21 -0500

    feat: scope for finding all reactions belonging to given rich text(s)

--- spec/models/rich_text_reactions_spec.rb
+++ spec/models/rich_text_reactions_spec.rb
@@ -15,34 +15,40 @@ RSpec.describe RichTextReaction do
     expect(rich_text_reaction.errors[:emoji_caption]).to be_present
   end
 
-  describe '.reactions' do
-    let!(:standup_meeting) { create(:standup_meeting) }
-    let!(:rich_text) { standup_meeting.yesterday_work_description }
+  describe '.for_rich_text' do
+    context 'when given an ActionText::RichText record' do
+      it 'returns all associated RichTextReaction records' do
+        # Create a couple of reactions for the same rich text.
+        reaction = create(:rich_text_reaction)
+        create(:rich_text_reaction,
+          user: create(:user),
+          rich_text_id: reaction.rich_text_id)  
 
-    it 'returns all reaction info for an update' do
-      # Emulate a user reacting to her own update, and another user reacting to
-      # the first user's update.
-      user = standup_meeting.user
-      other_user = create(:user)
-      my_reaction = Emoji.captions.first
-      other_reaction = Emoji.captions.second
-      described_class.create(emoji_caption: my_reaction, user:, rich_text:)
-      described_class.create(
-        emoji_caption: other_reaction,
-        user: other_user,
-        rich_text:
-      )
+        reactions = described_class.for_rich_text(reaction.rich_text)
 
-      reactions = described_class.reactions(rich_text)
-
-      # Output should tell who made which reaction.
-      expected_reactions = { my_reaction => [user.id], other_reaction => [other_user.id] }
-      expect(reactions).to eq(expected_reactions)
+        expect(described_class.all - reactions).to be_empty
+      end
     end
+  end
 
-    it 'returns an empty hash if an update has no reactions' do
-      reactions = described_class.reactions(rich_text)
-      expect(reactions).to be_empty
+  describe '.for_rich_texts' do
+    context 'when given multiple ActionText::RichText ids' do
+      it 'returns all associated RichTextReaction records' do
+        # Create a couple of reactions for each rich text.
+        100.times do
+          reaction = create(:rich_text_reaction)
+          create(:rich_text_reaction,
+            user: create(:user),
+            rich_text_id: reaction.rich_text_id)  
+        end
+
+        # The ids of the rich texts to find reactions for.
+        rich_text_ids = described_class.distinct.pluck(:rich_text_id)
+
+        reactions = described_class.for_rich_texts(rich_text_ids)
+
+        expect(described_class.all - reactions).to be_empty
+      end
     end
   end
 end
