dbaf2087bffef6eb7860bbd05a76406f692086d9
Won Rhim <wwrk22@gmail.com>
2024-01-02 14:54:34 -0500

    refactor: eliminate view component query and do some clean up

--- app/components/standup_meeting/meeting_update_component.rb
+++ app/components/standup_meeting/meeting_update_component.rb
@@ -6,15 +6,16 @@ class StandupMeeting::MeetingUpdateComponent < ViewComponent::Base
   def initialize(standup_meeting:, content_type:)
     @standup_meeting = standup_meeting
     @content_type = content_type
-    @user = standup_meeting.user
   end
 
   private
 
-  attr_reader :standup_meeting, :content_type, :user
-
   def standup_content
-    @standup_content ||= standup_meeting.public_send(content_type)
+    @standup_content ||= @standup_meeting.public_send(@content_type)
+  end
+
+  def user_full_name
+    @standup_meeting.user.full_name
   end
 
   def render?
