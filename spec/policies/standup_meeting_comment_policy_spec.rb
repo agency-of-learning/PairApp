bb8e9684922232306d375483ebf8d20faf568fe3
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2024-01-09 09:31:36 -0800

    Add StandupMeetingCommentPolicy edit? spec

--- spec/policies/standup_meeting_comment_policy_spec.rb
+++ spec/policies/standup_meeting_comment_policy_spec.rb
@@ -8,8 +8,14 @@ RSpec.describe StandupMeetingCommentPolicy, type: :policy do
   let(:not_owned_comment) { build(:standup_meeting_comment) }
   let(:authored_comment) { build(:standup_meeting_comment, user: member) }
 
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  permissions :edit? do
+    it 'denies access if user is not author of comment' do
+      expect(subject).not_to permit(member, not_owned_comment)
+    end
+
+    it 'grants access if user is author of comment' do
+      expect(subject).to permit(member, authored_comment)
+    end
   end
 
   permissions :destroy? do
