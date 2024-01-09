008127f53fa46015418ed9cf454800332b14700d
Won Rhim <wwrk22@gmail.com>
2024-01-09 13:18:20 -0500

    chore: add test for code coverage

--- spec/lib/emoji_spec.rb
+++ spec/lib/emoji_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe Emoji do
       expect(subject.emoji).to eq(Emoji::DICTIONARY[caption])
     end
   end
+
+  describe '.captions' do
+    it 'returns the set of allowed emoji captions' do
+      expect(described_class.captions).to eq(described_class::DICTIONARY.keys)
+    end
+  end
+
+  describe '.emojis' do
+    it 'returns the set of allowed emojis' do
+      expect(described_class.emojis).to eq(described_class::DICTIONARY.values)
+    end
+  end
 end
