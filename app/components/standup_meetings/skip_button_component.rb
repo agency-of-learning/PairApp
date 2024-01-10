683c8f6082db1eeba256133eed18579d61188413
Won Rhim <wwrk22@gmail.com>
2024-01-10 13:22:48 -0500

    refactor: pluralize namespaces for consistency and to avoid flooding model class namespace - Removed template for which there is no CheckInCtaComponent class defined.

--- app/components/standup_meetings/skip_button_component.rb
+++ app/components/standup_meetings/skip_button_component.rb
@@ -1,33 +1,35 @@
-class StandupMeetings::SkipButtonComponent < ViewComponent::Base
-  STATUS_POLICIES = {
-    skip: StandupMeetings::SkipPolicy
-  }.freeze
+module StandupMeetings
+  class SkipButtonComponent < ViewComponent::Base
+    STATUS_POLICIES = {
+      skip: StandupMeetings::SkipPolicy
+    }.freeze
 
-  def initialize(standup_meeting:, standup_meeting_group:, current_user:)
-    @standup_meeting = standup_meeting
-    @standup_meeting_group = standup_meeting_group
-    @user = current_user
-  end
+    def initialize(standup_meeting:, standup_meeting_group:, current_user:)
+      @standup_meeting = standup_meeting
+      @standup_meeting_group = standup_meeting_group
+      @user = current_user
+    end
 
-  def call
-    button_to(
-      'Skip',
-      standup_meeting_group_standup_meeting_skips_path(standup_meeting_group, standup_meeting),
-      class: 'btn btn-link',
-      form_class: 'w-fit'
-    )
-  end
+    def call
+      button_to(
+        'Skip',
+        standup_meeting_group_standup_meeting_skips_path(standup_meeting_group, standup_meeting),
+        class: 'btn btn-link',
+        form_class: 'w-fit'
+      )
+    end
 
-  private
+    private
 
-  attr_reader :user, :standup_meeting, :standup_meeting_group
+    attr_reader :user, :standup_meeting, :standup_meeting_group
 
-  def policy(action)
-    STATUS_POLICIES.fetch(action).new(user, standup_meeting)
-  end
+    def policy(action)
+      STATUS_POLICIES.fetch(action).new(user, standup_meeting)
+    end
 
-  def render?
-    !(standup_meeting.skipped? || standup_meeting.completed?) &&
-      policy(:skip).create? && standup_meeting.persisted?
+    def render?
+      !(standup_meeting.skipped? || standup_meeting.completed?) &&
+        policy(:skip).create? && standup_meeting.persisted?
+    end
   end
 end
