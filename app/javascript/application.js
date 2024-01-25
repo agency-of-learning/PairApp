021be18f73360a760305ae513a4a7bc02cf465d9
Won Rhim <wwrk22@gmail.com>
2024-01-25 16:13:26 -0500

    feat: hide emoji selector when clicking away

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,6 +5,7 @@ import 'trix';
 import '@rails/actiontext';
 import './src/direct_uploads';
 import './src/inline_code';
+import './src/emoji_selector';
 
 import * as ActiveStorage from '@rails/activestorage';
 
---
+++ app/javascript/src/emoji_selector.js
@@ -0,0 +1,13 @@
+document.addEventListener('click', function(event) {
+	const emojiSelections = document.querySelectorAll('details[open].emoji-selection');
+
+	if (emojiSelections.length > 0) {
+		const selectionClicked = [...emojiSelections].some(
+			selection => selection.contains(event.target)
+		);
+
+		if (selectionClicked === false) {
+			emojiSelections.forEach(selection => selection.removeAttribute('open'));
+		}
+	}
+});
