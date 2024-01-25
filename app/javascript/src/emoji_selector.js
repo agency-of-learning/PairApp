832fff665dd16e7715dbef6c8030c31b8f0c00a1
Won Rhim <wwrk22@gmail.com>
2024-01-25 17:23:01 -0500

    ESLint

--- app/javascript/src/emoji_selector.js
+++ app/javascript/src/emoji_selector.js
@@ -1,13 +1,13 @@
-document.addEventListener('click', function(event) {
-	const emojiSelections = document.querySelectorAll('details[open].emoji-selection');
+document.addEventListener('click', (event) => {
+  const emojiSelections = document.querySelectorAll('details[open].emoji-selection');
 
-	if (emojiSelections.length > 0) {
-		const selectionClicked = [...emojiSelections].some(
-			selection => selection.contains(event.target)
-		);
+  if (emojiSelections.length > 0) {
+    const selectionClicked = [...emojiSelections].some(
+      (selection) => selection.contains(event.target),
+    );
 
-		if (selectionClicked === false) {
-			emojiSelections.forEach(selection => selection.removeAttribute('open'));
-		}
-	}
+    if (selectionClicked === false) {
+      emojiSelections.forEach((selection) => selection.removeAttribute('open'));
+    }
+  }
 });
