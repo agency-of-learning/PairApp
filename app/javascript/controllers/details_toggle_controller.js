88a21c19daf8c275ebbb34e3b0938af087811c09
Won Rhim <wwrk22@gmail.com>
2024-01-29 11:26:36 -0500

    chore: eslint

--- app/javascript/controllers/details_toggle_controller.js
+++ app/javascript/controllers/details_toggle_controller.js
@@ -1,13 +1,13 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="details-toggle"
 export default class extends Controller {
-	static targets = ["details"];
+  static targets = ['details'];
 
-	clickOut(event) {
-		const detailsIsOpen = this.detailsTarget.hasAttribute('open');
-		const clickedOutOfDetails = this.detailsTarget.contains(event.target) === false;
+  clickOut(event) {
+    const detailsIsOpen = this.detailsTarget.hasAttribute('open');
+    const clickedOutOfDetails = this.detailsTarget.contains(event.target) === false;
 
-		if (detailsIsOpen && clickedOutOfDetails) this.detailsTarget.removeAttribute('open');
-	}
+    if (detailsIsOpen && clickedOutOfDetails) this.detailsTarget.removeAttribute('open');
+  }
 }
