7ef63c948ecedf2f1fb747fa47c11e49fb08546f
Josh Smith <jmsmith1018@gmail.com>
2024-02-14 11:20:25 -0800

    Unschedule job for creating standup meetings

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -13,10 +13,11 @@ s.every '30m' do
   PairRequests::AutoExpireWorker.perform_async
 end
 
+# NOTE: job unscheduled until storage issues are resolved
 # This does a longer look ahead to account for failures and ensure nothing is missed.
-s.every '15m' do
-  StandupMeetingGroups::DetermineMissingStandupMeetingsWorker.perform_async(0, 60)
-end
+# s.every '15m' do
+#   StandupMeetingGroups::DetermineMissingStandupMeetingsWorker.perform_async(0, 60)
+# end
 
 s.every '24h' do
   StandupMeetings::AutoMissedMeetingWorker.perform_async
